94e95a49f0f93040712ed72fcab951272261c3c6
alexlittle <alex@alexlittle.net>
2013-06-25 19:27:00 +0200

    trying to get RTD docs working still...

--- docs/conf.py
+++ docs/conf.py
@@ -13,13 +13,18 @@
 
 import sys, os
 
-#sys.path.append(os.path.abspath('../../'))
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'home_alexlittle_net.settings'
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.abspath('../oppia/'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('../oppia/'))
+import django 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+if django.VERSION < (1, 4):
+    from django.core.management import setup_environ
+    settings = __import__(os.environ["DJANGO_SETTINGS_MODULE"])
+    setup_environ(settings)
 
 # -- General configuration -----------------------------------------------------
 
