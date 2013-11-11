d90ab267c5a951738d7f6765f759ec43a9d011e7
alexlittle <alex@alexlittle.net>
2013-11-11 13:22:29 +0100

    CLOSED - task 67: Fix raw_post deprecation warning in api/resources.py https://github.com/DigitalCampus/django-oppia/issues/issue/67

--- oppia/api/resources.py
+++ oppia/api/resources.py
@@ -267,7 +267,7 @@ class TrackerResource(ModelResource):
     
     def patch_list(self,request,**kwargs):
         request = convert_post_to_patch(request)
-        deserialized = self.deserialize(request, request.raw_post_data, format=request.META.get('CONTENT_TYPE', 'application/json'))
+        deserialized = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
         for data in deserialized["objects"]:
             data = self.alter_deserialized_detail_data(request, data)
             bundle = self.build_bundle(data=dict_strip_unicode_keys(data))
