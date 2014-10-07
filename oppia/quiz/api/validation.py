79b11e8ac4bfe96eda8c8afecd2bbb3ecc04f3eb
alexlittle <alex@alexlittle.net>
2014-10-07 16:11:30 +0200

    All is_staff users to add to quiz props

--- oppia/quiz/api/validation.py
+++ oppia/quiz/api/validation.py
@@ -12,8 +12,9 @@ class QuizOwnerValidation(Validation):
             return {'__all__': 'no data.'}
         errors = {}
         quiz = bundle.obj.quiz
-        if quiz.owner.id != bundle.request.user.id:
-            errors['error_message'] = _(u"You are not the owner of this quiz")
+        if not bundle.request.user.is_staff:
+            if quiz.owner.id != bundle.request.user.id:
+                errors['error_message'] = _(u"You are not the owner of this quiz")
         return errors
     
 class QuestionOwnerValidation(Validation):
