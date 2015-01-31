00338c1e93ff9f49fba177a5cbada0bcc86b6138
alexlittle <alex@alexlittle.net>
2015-01-31 19:48:42 +0100

    OPEN - task 188: Runtime error in apache logs about native datetime https://github.com/DigitalCampus/django-oppia/issues/issue/188

--- oppia/viz/views.py
+++ oppia/viz/views.py
@@ -10,6 +10,7 @@ from django.db.models import Count, Sum, Q
 
 from django.contrib.auth import (authenticate, logout, views)
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from oppia.forms import DateDiffForm
 from oppia.models import Tracker, Course
@@ -20,7 +21,7 @@ def summary_view(request):
     if not request.user.is_staff:
          raise Http404
 
-    start_date = datetime.datetime.now() - datetime.timedelta(days=365)
+    start_date = timezone.now() - datetime.timedelta(days=365)
     if request.method == 'POST':
         form = DateDiffForm(request.POST)
         if form.is_valid():
@@ -94,7 +95,7 @@ def summary_view(request):
     
     previous_course_activity = Tracker.objects.filter(user__is_staff=False, submitted_date__lt=start_date).count()
                         
-    last_month = datetime.datetime.now() - datetime.timedelta(days=31)
+    last_month = timezone.now() - datetime.timedelta(days=31)
     hit_by_course = Tracker.objects.filter(user__is_staff=False, submitted_date__gte=last_month).exclude(course_id=None).values('course_id').annotate(total_hits=Count('id')).order_by('-total_hits')
     total_hits = Tracker.objects.filter(user__is_staff=False, submitted_date__gte=last_month).exclude(course_id=None).aggregate(total_hits=Count('id'))
     
