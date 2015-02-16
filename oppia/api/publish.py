b843a0c21fa2433746879a8909b022fa2d017362
alexlittle <alex@alexlittle.net>
2015-02-16 14:54:05 +0100

    OPEN - task 170: How to publish directly from Moodle https://github.com/DigitalCampus/django-oppia/issues/issue/170

--- oppia/api/publish.py
+++ oppia/api/publish.py
@@ -9,6 +9,7 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 
 from oppia.uploader import handle_uploaded_file
+from oppia.models import Tag, CourseTag
 from django.contrib import messages
 
 @csrf_exempt
@@ -17,11 +18,17 @@ def publish_view(request):
     if request.method != 'POST':
         return HttpResponse(status=405)
     
-    # authenticate user
-    if 'username' not in request.POST or 'password' not in request.POST or 'course_file' not in request.FILES:
-        return HttpResponse(status=400)
+    required = ['username','password','tags','is_draft']
    
-    # TODO - add tags & is_draft
+    for r in required:
+        if r not in request.POST:
+            return HttpResponse(status=400)
+   
+    
+    if 'course_file' not in request.FILES:
+        return HttpResponse(status=400)
+        
+    # authenticate user
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
@@ -34,5 +41,28 @@ def publish_view(request):
     if course is False:
         return HttpResponse(status=500)
     else:
+        print bool(request.POST['is_draft'])
+        course.is_draft = request.POST['is_draft']
+        course.save()
+        
+        tags = request.POST['tags'].strip().split(",")
+        # add tags
+        for t in tags:
+            try: 
+                tag = Tag.objects.get(name__iexact=t.strip())
+            except Tag.DoesNotExist:
+                tag = Tag()
+                tag.name = t.strip()
+                tag.created_by = user
+                tag.save()
+            # add tag to course
+            try:
+                ct = CourseTag.objects.get(course=course,tag=tag)
+            except CourseTag.DoesNotExist:
+                ct = CourseTag()
+                ct.course = course
+                ct.tag = tag
+                ct.save()
+        
         return HttpResponse(status=201)
     
