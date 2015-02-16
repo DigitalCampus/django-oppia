46046ab71d59530b32866659158f8b3211b40e26
alexlittle <alex@alexlittle.net>
2015-02-16 14:29:29 +0100

    OPEN - task 170: How to publish directly from Moodle https://github.com/DigitalCampus/django-oppia/issues/issue/170

--- oppia/api/publish.py
+++ oppia/api/publish.py
@@ -14,32 +14,25 @@ from django.contrib import messages
 @csrf_exempt
 def publish_view(request):
     
-    if request.method is not 'POST':
-        raise Http404
+    if request.method != 'POST':
+        return HttpResponse(status=405)
     
     # authenticate user
-    if 'username' not in request.POST or 'password' not in request.POST:
-        raise Http404
-    
-    if 'course_file' not in request.FILES:
-        raise Http404
+    if 'username' not in request.POST or 'password' not in request.POST or 'course_file' not in request.FILES:
+        return HttpResponse(status=400)
    
     # TODO - add tags & is_draft
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(username=username, password=password)
     if user is None or not user.is_active:
-        raise Http404
+        return HttpResponse(status=401)
             
     extract_path = os.path.join(settings.COURSE_UPLOAD_DIR,'temp',str(user.id))
     course = handle_uploaded_file(request.FILES['course_file'], extract_path, request, user)
 
     if course is False:
-        raise Http404
-   
+        return HttpResponse(status=500)
+    else:
+        return HttpResponse(status=201)
     
-        
-    return render_to_response('oppia/server.html',  
-                              {'settings': settings}, 
-                              content_type="application/json", 
-                              context_instance=RequestContext(request))
