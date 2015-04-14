0b93e6e0e958b460470225f0d0df72a2e4220ff7
alexlittle <alex@alexlittle.net>
2015-04-14 10:24:15 -0400

    fix error in uploading

--- oppia/views.py
+++ oppia/views.py
@@ -141,7 +141,7 @@ def upload_step1(request):
     if request.method == 'POST':
         form = UploadCourseStep1Form(request.POST,request.FILES)
         if form.is_valid(): # All validation rules pass
-            extract_path = os.join.path(settings.COURSE_UPLOAD_DIR, 'temp', str(request.user.id))
+            extract_path = os.path.join(settings.COURSE_UPLOAD_DIR, 'temp', str(request.user.id))
             course = handle_uploaded_file(request.FILES['course_file'], extract_path, request, request.user)
             if course:
                 return HttpResponseRedirect(reverse('oppia_upload2', args=[course.id])) # Redirect after POST
