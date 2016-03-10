80f3e8942bad5077f6a56d897d719a714f67d3de
Joseba S <jjoseba@gmail.com>
2016-03-10 20:41:59 +0100

    #286: Added view to view the device list

--- oppia/deviceadmin/models.py
+++ oppia/deviceadmin/models.py
@@ -8,6 +8,4 @@ from gcm.models import AbstractDevice
 class UserDevice(AbstractDevice):
     user = models.ForeignKey(User)
     model_name = models.TextField(blank=True, null=True, default=None)
-    admin_enabled = models.BooleanField(blank=False, default=False)
-
     
--- oppia/deviceadmin/views.py
+++ oppia/deviceadmin/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
+from django.template import RequestContext
+
+from oppia.deviceadmin.models import UserDevice
+
+
+def user_devices_list(request):
+    if not request.user.is_staff:
+        return HttpResponse('Unauthorized', status=401)
+
+    devices = UserDevice.objects.all()
+
+    return render_to_response('oppia/deviceadmin/list.html',
+                              { 'devices': devices },
+                              context_instance=RequestContext(request))
