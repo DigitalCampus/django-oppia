e36685b4372703984c7c63cec0109cbb7893926b
Joseba S <jjoseba@gmail.com>
2016-04-18 18:14:30 +0200

    #302: Finished styling search form

--- oppia/profile/forms.py
+++ oppia/profile/forms.py
@@ -13,7 +13,7 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Button, Layout, Fieldset, ButtonHolder, Submit, Div, HTML, Field, Row
+from crispy_forms.layout import Button, Layout, Fieldset, ButtonHolder, Submit, Div, HTML, Field, Row, Column
 
 
 class LoginForm(forms.Form):
@@ -298,19 +298,27 @@ class UserSearchForm(forms.Form):
     is_staff = forms.BooleanField(initial=False)
 
     register_start_date = forms.CharField(required=False,label=False)
-
     register_end_date = forms.CharField(required=False,label=False)
 
     def __init__(self, *args, **kwargs):
         super(UserSearchForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.fields['register_start_date'].label = False
         self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-sm-3 col-md-2 col-lg-2 control-label'
+        self.helper.field_class = 'col-sm-8 col-md-5 col-lg-5'
+        self.helper.form_method = 'GET'
         self.helper.layout = Layout(
-                'first_name','last_name','is_active','is_staff',
+                'first_name','last_name',
+                Row(Div('is_active', css_class='col-sm-4'),Div('is_staff',css_class='col-sm-4')),
                 Row(
-                    Div(Field('register_start_date',css_class='date-picker-input'),css_class='date-picker-row-fluid'),
-                    Div(Field('register_end_date',css_class='date-picker-input'),css_class='date-picker-row-fluid'),
-                    css_class='date-picker-row-fluid'
+                    Column(HTML('<label>Register date</label>'), css_class=self.helper.label_class),
+                    Div(HTML('<div class="btn hidden-xs disabled">from</div>'),css_class='date-picker-row-fluid'),
+                    Column(Field('register_start_date',css_class='date-picker-input'),css_class='date-picker-row-fluid'),
+                    Div(HTML('<div class="btn hidden-xs disabled">to</div>'),css_class='date-picker-row-fluid'),
+                    Column(Field('register_end_date',css_class='date-picker-input'),css_class='date-picker-row-fluid'),
                 ),
+                Div(
+                   Submit('submit', _('Search'), css_class='btn btn-default'),
+                   css_class='col-lg-7 col-md-7 col-sm-11 text-right',
+                )
             )
