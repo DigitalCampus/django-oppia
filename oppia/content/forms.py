f7b123c8c4125ee88ea72c6045a67793c557c375
alexlittle <alex@digital-campus.org>
2016-05-02 15:25:53 +0300

    update docs and media embed

--- oppia/content/forms.py
+++ oppia/content/forms.py
@@ -8,20 +8,20 @@ from crispy_forms.bootstrap import FieldWithButtons
 from crispy_forms.layout import Button, Layout, Fieldset, ButtonHolder, Submit, Div, HTML, Row
 
 
-class VideoEmbedHelperForm(forms.Form):
-    video_url = forms.CharField(
-                help_text=_("The url to your video - this should be a url to download the actual video file (.avi, .m4v or .mp4) , NOT a link to video streaming service such as YouTube, Vimeo etc"),
+class MediaEmbedHelperForm(forms.Form):
+    media_url = forms.CharField(
+                help_text=_("The url to your media - this should be a url to download the actual media file (.avi, .m4v/.mp4 or .mp3) , NOT a link to media streaming service such as YouTube, Vimeo etc"),
                 required=True,
-                error_messages={'required': _('Please enter the video url')},)
+                error_messages={'required': _('Please enter the media url')},)
     
     def __init__(self, *args, **kwargs):
-        super(VideoEmbedHelperForm, self).__init__(*args, **kwargs)
+        super(MediaEmbedHelperForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_class = 'form-horizontal'
         self.helper.label_class = 'col-lg-2'
         self.helper.field_class = 'col-lg-4'
         self.helper.layout = Layout(
-                'video_url',
+                'media_url',
                 Div(
                    Submit('submit', _(u'Go'), css_class='btn btn-default'),
                    css_class='col-lg-offset-2 col-lg-4',
@@ -29,6 +29,6 @@ class VideoEmbedHelperForm(forms.Form):
             )  
     
     def clean(self):
-        cleaned_data = super(VideoEmbedHelperForm, self).clean()
+        cleaned_data = super(MediaEmbedHelperForm, self).clean()
         
         return cleaned_data
