a09492448551ee34514f9b7c68d0dd85b38348c7
Joseba S <jjoseba@gmail.com>
2016-05-10 20:02:09 +0200

    #323: Switched downloads/activities (they were wrong) calculation, and added an extra comprobation to start from zero if no last tracker pk is passed

--- oppia/summary/models.py
+++ oppia/summary/models.py
@@ -1,12 +1,11 @@
 import time
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import Sum
 from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
-from oppia.models import Course, Tracker, Activity, Points
-from oppia.quiz.models import Quiz, QuizAttempt
+from oppia.models import Course, Tracker, Points
 
 
 class UserCourseSummary (models.Model):
@@ -30,23 +29,27 @@ class UserCourseSummary (models.Model):
 
     def update_summary(self, last_tracker_pk=0, last_points_pk=0):
 
+        ltpk = last_tracker_pk
+        lppk = last_points_pk
+
         t = time.clock()
         selfTrackers = Tracker.objects.filter(user=self.user, course=self.course, pk__gt=last_tracker_pk)
 
         ### Add the values that are directly obtained from the last pks
-        self.total_downloads += selfTrackers.count()
-        self.total_activity += selfTrackers.filter(type='download').count()
+        self.total_activity  = (0 if ltpk == 0 else self.total_activity) + selfTrackers.count()
+        self.total_downloads = (0 if ltpk == 0 else self.total_activity) + selfTrackers.filter(type='download').count()
         new_points = Points.objects.filter(pk__gt=last_points_pk, course=self.course,user=self.user)\
                                      .aggregate(total=Sum('points'))['total']
         if new_points:
-            self.points += new_points
+            self.points += (0 if lppk == 0 else self.points) + new_points
         ### Values that need to be recalculated (as the course digests may vary)
         self.pretest_score = Course.get_pre_test_score(self.course, self.user)
         self.quizzes_passed = Course.get_no_quizzes_completed(self.course, self.user)
         self.badges_achieved = Course.get_badges(self.course, self.user)
         self.completed_activities = Course.get_activities_completed(self.course, self.user)
-        self.save()
 
+        ### Update the data in the database
+        self.save()
 
         elapsed_time = time.clock() - t
         print(self)
