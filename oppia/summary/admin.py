a3f9ca692250c0dddf7cf8dc54a4ea8f8981a181
Joseba S <jjoseba@gmail.com>
2016-07-26 13:32:43 +0200

    #372: Added admin actions to update summaries

--- oppia/summary/admin.py
+++ oppia/summary/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from oppia.summary.models import UserCourseSummary, CourseDailyStats, SettingProperties
 
 
-
+def message_user(model, request, model_name, query_count):
+    if query_count == 1:
+         model.message_user(request, model_name + " summary succesfully updated.")
+    elif query_count > 0:
+        model.message_user(request, model_name + " summaries succesfully updated.")
 
 class UserCourseSummaryAdmin(admin.ModelAdmin):
     list_display = ('user', 'course', 'points', 'total_downloads', 'total_activity', 'quizzes_passed',
@@ -13,11 +17,21 @@ class UserCourseSummaryAdmin(admin.ModelAdmin):
     def update_summary(self, request, queryset):
         for courseSummary in queryset:
             courseSummary.update_summary()
+        message_user(self, request, "User-course", queryset.count())
 
     update_summary.short_description = "Update summary"
 
 class CourseDailyStatsAdmin(admin.ModelAdmin):
     list_display = ('course', 'day', 'type', 'total')
+
+    actions = ['update_summary']
+
+    def update_summary(self, request, queryset):
+        for dailyStats in queryset:
+            CourseDailyStats.update_daily_summary(dailyStats.course.id, dailyStats.day)
+        message_user(self, request, "Daily stats", queryset.count())
+
+    update_summary.short_description = "Update summary"
 
 
 admin.site.register(UserCourseSummary, UserCourseSummaryAdmin)
