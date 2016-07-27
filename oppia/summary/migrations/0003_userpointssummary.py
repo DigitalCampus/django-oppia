edbb1c318fcdd00befea4d4ada1b88f1bd4a62a8
Joseba S <jjoseba@gmail.com>
2016-07-27 05:04:57 +0200

    #370: Squashed migrations into a single one

--- oppia/summary/migrations/0003_userpointssummary.py
+++ oppia/summary/migrations/0003_userpointssummary.py
@@ -18,6 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('points', models.IntegerField(default=0)),
+                ('badges', models.IntegerField(default=0)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
