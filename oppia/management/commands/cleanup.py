6884d657e7d2862f6c394d2f1bfb241560134a13
Joseba S <jjoseba@gmail.com>
2016-09-26 14:53:39 +0200

    #349: Added all queries for unused elements and prompt the user with a summary before attempting the deletion

--- oppia/management/commands/cleanup.py
+++ oppia/management/commands/cleanup.py
@@ -1,7 +1,9 @@
-from django.core.management.base import BaseCommand, CommandError
+from distutils.util import strtobool
+
+from django.core.management.base import BaseCommand
 
 from oppia.models import Activity
-from oppia.quiz.models import Quiz, QuizProps, QuizAttempt
+from oppia.quiz.models import Quiz, QuizProps, QuizAttempt, Response, ResponseProps, Question, QuestionProps
 
 
 class Command(BaseCommand):
@@ -10,22 +12,48 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         pass
 
+
+    def prompt(self, query):
+       self.stdout.write('%s [y/n]: ' % query)
+       val = raw_input()
+       try:
+           ret = strtobool(val)
+       except ValueError:
+           self.stdout.write('Please answer with a y/n\n')
+           return self.prompt(query)
+       return ret
+
     def handle(self, *args, **options):
 
-        quiz_activities = Activity.objects.filter(type=Activity.QUIZ).values_list('digest', flat=True)
+        quiz_act_digests = Activity.objects.filter(type=Activity.QUIZ).values_list('digest', flat=True)
         quiz_old_digests = QuizProps.objects.filter(name="digest")\
-                            .exclude(value__in=quiz_activities)\
+                            .exclude(value__in=quiz_act_digests)\
                             .values_list('value', flat=True)
 
         quizzes = Quiz.objects.filter(quizprops__value__in=quiz_old_digests, quizprops__name="digest")
         quizzes_attempts = QuizAttempt.objects.all().values('quiz').distinct()
-        quizzes_without_attempts = quizzes.exclude(pk__in=quizzes_attempts)
 
-        print Quiz.objects.all().count()
-        print quiz_activities.count()
-        print quiz_old_digests.count()
-        print quizzes.count()
-        print quizzes_attempts.count()
-        print quizzes_without_attempts.count()
+        to_delete = {}
+        to_delete['Quizzes'] = quizzes.exclude(pk__in=quizzes_attempts) # quizzes without attempts
+        to_delete['QuizProps'] = QuizProps.objects.filter(quiz__in=to_delete['Quizzes'])
+        to_delete['Questions'] = Question.objects.filter(quizquestion__quiz__in=to_delete['Quizzes'])
+        to_delete['QuestionProps'] = QuestionProps.objects.filter(question__in=to_delete['Questions'])
+        to_delete['Responses'] = Response.objects.filter(question__in=to_delete['Questions'])
+        to_delete['ResponseProps'] = ResponseProps.objects.filter(response__in=to_delete['Responses'])
 
-        raise CommandError('Poll does not exist')
+        self.stdout.write(self.style.MIGRATE_HEADING("Summary of deletions:"))
+        sum = 0
+        for key in to_delete:
+            elem_count = to_delete[key].count()
+            sum += elem_count
+            self.stdout.write(
+                self.style.MIGRATE_LABEL("  * "+key+":") + ' %d items to delete' % (elem_count))
+
+        if sum is 0:
+            self.stdout.write(self.style.MIGRATE_SUCCESS("No new elements to clean up."))
+        else:
+            if self.prompt("You are about to delete %d records, are you sure?" % sum):
+                for key in to_delete:
+                    to_delete[key].delete()
+                self.stdout.write(self.style.MIGRATE_SUCCESS("Quizzes cleaned up :)"))
+
