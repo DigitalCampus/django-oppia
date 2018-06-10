69b0159e9df891b8c42c9e954339953c055d30e5
Alex Little <alex@digital-campus.org>
2018-06-11 01:48:30 +0300

    pep8ify content dir

--- oppia/content/urls.py
+++ oppia/content/urls.py
@@ -6,4 +6,4 @@ from oppia.content import views as oppia_content_views
 urlpatterns = [
                 url(r'^video-embed-helper/$', oppia_content_views.media_embed_helper, name="oppia_video_embed_helper"),
                 url(r'^media-embed-helper/$', oppia_content_views.media_embed_helper, name="oppia_media_embed_helper"),
-        ]      
+        ]
