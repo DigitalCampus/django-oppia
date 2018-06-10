2a548eebffc82c4cc7dc307fcb2fa670d7cbbb6d
Alex Little <alex@digital-campus.org>
2018-06-10 21:53:09 +0300

    pep8ify gamification code

--- oppia/gamification/views.py
+++ oppia/gamification/views.py
@@ -9,27 +9,28 @@ from django.utils import timezone
 from oppia.models import Points, Course
 from readline import get_begidx
 
+
 @staff_member_required
 def leaderboard_export(request, course_id=None):
-    
+
     if request.is_secure():
         prefix = 'https://'
     else:
         prefix = 'http://'
-            
+
     response_data = {}
     response_data['generated_date'] = timezone.now()
     response_data['server'] = prefix + request.META['SERVER_NAME']
-    
+
     if course_id is None:
         leaderboard = Points.get_leaderboard()
     else:
-        course = get_object_or_404(Course,pk=course_id)
+        course = get_object_or_404(Course, pk=course_id)
         leaderboard = Points.get_leaderboard(course=course)
         response_data['course'] = course.shortname
-    
+
     response_data['leaderboard'] = []
-    
+
     for idx, leader in enumerate(leaderboard):
         leader_data = {}
         leader_data['position'] = idx + 1
@@ -39,5 +40,5 @@ def leaderboard_export(request, course_id=None):
         leader_data['points'] = leader.total
         leader_data['badges'] = leader.badges
         response_data['leaderboard'].append(leader_data)
-    
+
     return JsonResponse(response_data)
