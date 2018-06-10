44fa728fb4c92d21f0609dce8440de95e78e6011
Alex Little <alex@digital-campus.org>
2018-06-11 01:36:48 +0300

    pep8ify activitylog dir

--- oppia/activitylog/views.py
+++ oppia/activitylog/views.py
@@ -1,4 +1,3 @@
-
 # oppia/tracker/views.py
 
 import json
@@ -18,26 +17,27 @@ from tastypie.models import ApiKey
 from oppia.activitylog.forms import UploadActivityLogForm
 from oppia.activitylog.models import UploadedActivityLog
 
+
 def upload_view(request):
     if not request.user.userprofile.get_can_upload_activitylog():
         raise exceptions.PermissionDenied
-    
-    if request.method == 'POST':    
-        form = UploadActivityLogForm(request.POST,request.FILES)
+
+    if request.method == 'POST':
+        form = UploadActivityLogForm(request.POST, request.FILES)
         if form.is_valid():
             activity_log_file = request.FILES["activity_log_file"]
-            
+
             # save activity_log_file
-            uploaded_activity_log = UploadedActivityLog(create_user = request.user,
+            uploaded_activity_log = UploadedActivityLog(create_user=request.user,
                                       file=activity_log_file)
             uploaded_activity_log.save()
-             
+
             # open file and process
             file_data = open(uploaded_activity_log.file.path, 'rb').read()
-            json_data = json.loads(file_data)    
-            
+            json_data = json.loads(file_data)
+
             # TODO check server matches
-            
+
             if 'users' in json_data:
                 for user in json_data['users']:
                     print(_(u"processing activity log for %s" % user['username']))
@@ -46,7 +46,7 @@ def upload_view(request):
                         if 'trackers' in user:
                             for tracker in user['trackers']:
                                 url_comp = request.build_absolute_uri().split('/')
-                                url = ('%(protocol)s//%(domain)s/api/v1/tracker/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key })
+                                url = ('%(protocol)s//%(domain)s/api/v1/tracker/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key})
                                 data = json.dumps(tracker)
                                 req = urllib2.Request(url, data)
                                 req.add_header('Content-Type', 'application/json; charset=utf-8')
@@ -55,11 +55,11 @@ def upload_view(request):
                                     urllib2.urlopen(req)
                                     messages.info(request, _(u"Tracker activity for %(username)s added" % {'username': user['username']}))
                                 except urllib2.HTTPError:
-                                    messages.warning(request, _(u"Already uploaded: tracker activity for %(username)s added" % {'username': user['username']}),'danger')
+                                    messages.warning(request, _(u"Already uploaded: tracker activity for %(username)s added" % {'username': user['username']}), 'danger')
                         if 'quizresponses' in user:
                             for quizattempt in user['quizresponses']:
                                 url_comp = request.build_absolute_uri().split('/')
-                                url = ('%(protocol)s//%(domain)s/api/v1/quizattempt/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key })
+                                url = ('%(protocol)s//%(domain)s/api/v1/quizattempt/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key})
                                 data = json.dumps(quizattempt)
                                 req = urllib2.Request(url, data)
                                 req.add_header('Content-Type', 'application/json; charset=utf-8')
@@ -69,17 +69,15 @@ def upload_view(request):
                                     messages.info(request, _(u"Quiz attempt for %(username)s added" % {'username': user['username']}))
                                 except urllib2.HTTPError:
                                     messages.info(request, _(u"Already uploaded: quiz attempt for %(username)s added" % {'username': user['username']}))
-                                
+
                     except ApiKey.DoesNotExist:
-                        messages.warning(request, _(u"%(username)s not found. Please check that this file is being uploaded to the correct server." % {'username': user['username']}),'danger')
+                        messages.warning(request, _(u"%(username)s not found. Please check that this file is being uploaded to the correct server." % {'username': user['username']}), 'danger')
                         print(_(u"No user api key found for %s" % user['username']))
-                   
                                       
             return HttpResponseRedirect(reverse('oppia_activitylog_upload_success'))
     else:
         form = UploadActivityLogForm()
 
-    return render(request, 'oppia/activitylog/upload.html', 
+    return render(request, 'oppia/activitylog/upload.html',
                               {'form': form,
-                               'title':_(u'Upload Activity Log')})
-    
+                               'title': _(u'Upload Activity Log')})
