1595e38eed0f5ffa7e79224410f8182926c20513
Alex Little <alex@digital-campus.org>
2018-06-15 18:01:22 +0300

    reduce cognitive complexity of generate_media_images

--- oppia/av/management/commands/generate_media_images.py
+++ oppia/av/management/commands/generate_media_images.py
@@ -6,6 +6,7 @@ Management command to generate sample media images
 import os
 import subprocess
 import time
+import oppia.content
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -15,10 +16,6 @@ from django.core.management.base import BaseCommand, CommandError
 from django.utils.translation import ugettext_lazy as _
 
 from oppia.av.models import UploadedMedia, UploadedMediaImage
-
-DEFAULT_WIDTH = 320
-DEFAULT_HEIGHT = 180
-
 
 class Command(BaseCommand):
     help = _(u"Generates sample media images")
@@ -41,29 +38,37 @@ class Command(BaseCommand):
                 print("  > Created output dir " + cache_dir)
 
             print("  > Generating miniatures... \r", )
-            image_generator_command = "ffmpeg -i %s -r 0.02 -s %dx%d -f image2 %s/frame-%%03d.png" % (m.file.path, DEFAULT_WIDTH, DEFAULT_HEIGHT, cache_dir)
+            image_generator_command = "ffmpeg -i %s -r 0.02 -s %dx%d -f image2 %s/frame-%%03d.png" % (m.file.path, oppia.content.SCREENSHOT_IMAGE_WIDTH, oppia.content.SCREENSHOT_IMAGE_HEIGHT, cache_dir)
             ffmpeg = subprocess.Popen(image_generator_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-            current_frame = 0
-            for line in iter(ffmpeg.stdout.readline, ''):
-                if "frame=" in line:
-                    if line.split(" ")[3] is "":
-                        frame = int(line.split(" ")[4])
-                    else:
-                        frame = int(line.split(" ")[3])
-                    if frame > current_frame:
-                        current_frame = frame
-                        print("  > Generating miniatures... " + str(frame * 10) + "% \r", )
+            
+            self.process_ffmpeg_output(ffmpeg)
+            
             print("  > Generating miniatures... 100% \r\n", )
 
             # Now get the images generated and add to the db
-            image_file_list = os.listdir(cache_dir)
-            for filename in image_file_list:
-                print(filename)
-                media_image = UploadedMediaImage(create_user=m.create_user, uploaded_media=m)
-                data = None
-                with open(os.path.join(cache_dir, filename), 'rb') as f:
-                    data = f.read()
-                media_image.image.save(filename, ContentFile(data))
-                media_image.save()
+            self.add_images_to_db(cache_dir)
 
         print("\n  > Process completed.")
+        
+    def add_images_to_db(self, cache_dir):
+        image_file_list = os.listdir(cache_dir)
+        for filename in image_file_list:
+            print(filename)
+            media_image = UploadedMediaImage(create_user=m.create_user, uploaded_media=m)
+            data = None
+            with open(os.path.join(cache_dir, filename), 'rb') as f:
+                data = f.read()
+            media_image.image.save(filename, ContentFile(data))
+            media_image.save()
+            
+    def process_ffmpeg_output(self, ffmpeg):
+        current_frame = 0
+        for line in iter(ffmpeg.stdout.readline, ''):
+            if "frame=" in line:
+                if line.split(" ")[3] is "":
+                    frame = int(line.split(" ")[4])
+                else:
+                    frame = int(line.split(" ")[3])
+                if frame > current_frame:
+                    current_frame = frame
+                    print("  > Generating miniatures... " + str(frame * 10) + "% \r", )
