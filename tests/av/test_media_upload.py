29367ec8956c9efbb5214fbced875cbc03cae670
Alex Little <alex@digital-campus.org>
2019-01-25 20:47:57 +0200

    537: Add tests for the uploading media (via the webform)
    Task-Url: https://github.com/DigitalCampus/django-oppia/issues/issues/537

--- tests/av/test_media_upload.py
+++ tests/av/test_media_upload.py
@@ -1,4 +1,6 @@
 # tests/av/test_course_publish.py
+
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
@@ -6,6 +8,7 @@ from django.contrib.auth.models import User
 from tests.utils import *
 
 from av.forms import UploadMediaForm
+from av.models import UploadedMedia
 
 class MediaUploadResourceTest(TestCase):
     fixtures = ['user.json', 'oppia.json', 'quiz.json', 'permissions.json']
@@ -31,32 +34,18 @@ class MediaUploadResourceTest(TestCase):
         self.teacher_user = {
             'user': 'teacher',
             'password': 'password'
-        }
-    '''    
-    
-    TODO - find a way to test this reliably with the mime types, see: 
-    https://stackoverflow.com/questions/54366621/django-unit-testing-is-it-possible-to-specify-a-mime-type-for-file-upload
-    
+        }   
+
     def test_upload_template(self):
         
-        media_file = open(self.media_file_path,'rb') 
+        media_file_content = open(self.media_file_path,'rb') 
+        media_file = SimpleUploadedFile(media_file_content.name, media_file_content.read(), content_type="video/m4v")
         
         self.client.login(username=self.admin_user['user'], password=self.admin_user['password'])
-        
         response = self.client.post(reverse('oppia_av_upload'), {'media_file': media_file })
+        self.assertRedirects(response, reverse('oppia_av_upload_success', args=[1]), 302, 200)
         
-        self.assertEqual(response.status_code, 200)
-        
-        media_file.close()
+        media_file_content.close()
     
-            
-    def test_upload_form(self):
         
-        media_file = open(self.media_file_path,'rb') 
         
-        form_data = {'media_file': media_file}
-        form = UploadMediaForm(data=form_data)
-        self.assertTrue(form.is_valid())
-        
-        media_file.close()
-    '''
