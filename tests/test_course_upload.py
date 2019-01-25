f3edac8e4c4920a49bf7337bb050ec87fa64e36a
Alex Little <alex@digital-campus.org>
2019-01-25 19:57:16 +0200

    541: Add tests for uploading course via the webform
    Task-Url: https://github.com/DigitalCampus/django-oppia/issues/issues/541

---
+++ tests/test_course_upload.py
@@ -0,0 +1,58 @@
+# tests/av/test_course_publish.py
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+from tests.utils import *
+
+from oppia.forms.upload import UploadCourseStep1Form, UploadCourseStep2Form
+
+class CourseUploadTest(TestCase):
+    fixtures = ['user.json', 'oppia.json', 'quiz.json', 'permissions.json']
+    
+    course_file_path = './oppia/fixtures/reference_files/anc_course.zip' 
+    media_file_path = './oppia/fixtures/reference_files/sample_video.m4v'
+    
+    def setUp(self):
+        super(CourseUploadTest, self).setUp()
+
+        self.admin_user = {
+            'user': 'admin',
+            'password': 'password'
+        }
+        self.staff_user = {
+            'user': 'staff',
+            'password': 'password'
+        }
+        self.normal_user = {
+            'user': 'demo',
+            'password': 'password'
+        }
+        self.teacher_user = {
+            'user': 'teacher',
+            'password': 'password'
+        }
+    
+    def test_upload_template(self):
+        
+        course_file = open(self.course_file_path,'rb') 
+        
+        self.client.login(username=self.admin_user['user'], password=self.admin_user['password'])
+        response = self.client.post(reverse('oppia_upload'), {'course_file': course_file })        
+        self.assertRedirects(response, reverse('oppia_upload2', args=[1]), 302, 200) # should be redirected to the update step 2 form
+        
+        course_file.close()
+    
+    '''        
+    TODO - fix issue with how file upload mime types are recognised
+    
+    def test_upload_form(self):
+        
+        course_file = open(self.course_file_path,'rb') 
+        
+        form_data = {'course_file': course_file}
+        form = UploadCourseStep1Form(data=form_data)
+        self.assertTrue(form.is_valid())
+        
+        course_file.close()
+    '''
