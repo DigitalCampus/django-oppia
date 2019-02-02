9afe6cb7a673a01f8b85e11784d5ceeee85d8cf0
Alex Little <alex@digital-campus.org>
2019-02-03 00:13:53 +0200

    559: Add tests for profile urls....
    Task-Url: https://github.com/DigitalCampus/django-oppia/issues/issues/559

---
+++ tests/profile/views/test_view_search.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.urls import reverse
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+
+from tests.user_logins import *
+from tests.defaults import *
+
+class UserSearchActivityViewTest(TestCase):
+    fixtures = ['tests/test_user.json', 
+                'tests/test_oppia.json', 
+                'tests/test_quiz.json', 
+                'tests/test_permissions.json']
+
+    def setUp(self):
+        super(UserSearchActivityViewTest, self).setUp()
+        self.template = 'oppia/profile/search_user.html'
+        self.url = reverse('profile_search_users')
+        
+    def test_view_search(self):
+        
+        allowed_users = [ADMIN_USER, STAFF_USER]
+        disallowed_users = [TEACHER_USER, NORMAL_USER]
+        
+        for allowed_user in allowed_users:
+            self.client.login(username=allowed_user['user'], password=allowed_user['password'])
+            response = self.client.get(self.url)
+            self.assertTemplateUsed(response, self.template )
+            self.assertEqual(response.status_code, 200)
+            
+        for disallowed_user in disallowed_users:
+            self.client.login(username=disallowed_user['user'], password=disallowed_user['password'])
+            response = self.client.get(self.url)
+            self.assertRedirects(response, '/admin/login/?next=' +  self.url, 302, 200)
