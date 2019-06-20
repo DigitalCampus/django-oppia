94ad355dbaba032787003037781ebb4d176d6501
Alex Little <alex@digital-campus.org>
2019-06-20 13:38:20 +0300

    Remove unnecessary code and files (now that #628 is fixed)

--- gamification/urls.py
+++ gamification/urls.py
@@ -5,14 +5,7 @@ from gamification import views as oppia_gamification_views
 urlpatterns = [
     url(r'^leaderboard/export/server$', oppia_gamification_views.leaderboard_export, name="oppia_gamification_leaderboard_export_server"),
     url(r'^leaderboard/export/(?P<course_id>\d+)/$', oppia_gamification_views.leaderboard_export, name="oppia_gamification_leaderboard_export_course"),
-    url(r'^points/course/(?P<course_id>\d+)/edit/$', oppia_gamification_views.edit_course_points, name="oppia_gamification_edit_course_points"),
-    url(r'^points/course/(?P<course_id>\d+)/updated/$', oppia_gamification_views.course_points_updated, name="oppia_gamification_course_points_updated"),
-
+    
     url(r'^points/course/(?P<course_id>\d+)/$', oppia_gamification_views.edit_course_gamification, name="oppia_gamification_edit_course"),
 
-    url(r'^points/activity/(?P<course_id>\d+)/view/$', oppia_gamification_views.view_activity_points, name="oppia_gamification_view_activity_points"),
-    url(r'^points/activity/(?P<course_id>\d+)/edit/(?P<activity_id>\d+)/$', oppia_gamification_views.edit_activity_points, name="oppia_gamification_edit_activity_points"),
-    
-    url(r'^points/media/(?P<course_id>\d+)/view/$', oppia_gamification_views.view_media_points, name="oppia_gamification_view_media_points"),
-    url(r'^points/media/(?P<course_id>\d+)/edit/(?P<media_id>\d+)/$', oppia_gamification_views.edit_media_points, name="oppia_gamification_edit_media_points"),
 ]
