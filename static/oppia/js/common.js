80c7cc31c0d6ece9f4a80b0e56ac7fdb10b1017a
Joseba S <jjoseba@gmail.com>
2019-09-13 14:12:58 +0200

    #649: Added new date control to cohort and search pages

--- static/oppia/js/common.js
+++ static/oppia/js/common.js
@@ -16,23 +16,33 @@ $(function(){
 		if (picker.hasClass('single')){
 		    options['singleDatePicker'] = true;
 		    picker.daterangepicker(options);
-
 		}
         else{
+            options['autoUpdateInput'] = false;
             var startDate = picker.find('input[name="start_date"]');
             var endDate = picker.find('input[name="end_date"]');
             var dropDirection = picker.attr('data-drop');
 
-            if (startDate.val()){ options['startDate'] = startDate.val(); }
-            if (endDate.val()){ options['endDate'] = endDate.val(); }
-
-            startDate.val(moment(startDate.val(), datePickerFormat).format(datePickerFormat));
-            endDate.val(moment(endDate.val(), datePickerFormat).format(datePickerFormat));
-
+            if (startDate.val()){
+                options['startDate'] = startDate.val();
+                startDate.val(moment(startDate.val(), datePickerFormat).format(datePickerFormat));
+            }
+            if (endDate.val()){
+                options['endDate'] = endDate.val();
+                endDate.val(moment(endDate.val(), datePickerFormat).format(datePickerFormat));
+            }
             var daterange = picker.find('.daterange').daterangepicker(options, function(start, end, label){
                 startDate.val(start.format(datePickerFormat));
                 endDate.val(end.format(datePickerFormat));
+
             });
+            daterange.on('apply.daterangepicker', function(ev, picker) {
+                  'from' + $(this).val(picker.startDate.format(datePickerFormat) + ' -- ' + picker.endDate.format(datePickerFormat));
+            });
+
+            if (startDate.val() || endDate.val()){
+                daterange.val(startDate.val() + ' -- ' + endDate.val())
+            }
 
             picker.find('button').on('click', function(){ daterange.focus(); });
         }
