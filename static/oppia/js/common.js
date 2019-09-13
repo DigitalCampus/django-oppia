bfc805415183c7badb836391d713f755aef367c9
Joseba S <jjoseba@gmail.com>
2019-09-13 13:19:32 +0200

    #649: Use new date component in summary and activity detail pages

--- static/oppia/js/common.js
+++ static/oppia/js/common.js
@@ -4,30 +4,39 @@ $(function(){
     var datePickerFormat = 'YYYY-MM-DD';
 	$('.date-picker-selector').each(function(){
 		var picker = $(this);
-		var startDate = picker.find('input[name="start_date"]');
-    	var endDate = picker.find('input[name="end_date"]');
-    	var dropDirection = picker.attr('data-drop');
-    	options = {
-			cancelClass: 'btn-flat text-primary', drops:'up',
+        var dropDirection = picker.attr('data-drop') || 'down';
+        options = {
+			cancelClass: 'btn-flat text-primary',
 			applyClass: 'btn-light', drops:dropDirection,
 			locale:{
 				format:datePickerFormat
 			}
 		};
-		if (startDate.val()){ options['startDate'] = startDate.val(); }
-		if (endDate.val()){ options['endDate'] = endDate.val(); }
 
-        startDate.val(moment(startDate.val(), datePickerFormat).format(datePickerFormat));
-		endDate.val(moment(endDate.val(), datePickerFormat).format(datePickerFormat));
+		if (picker.hasClass('single')){
+		    options['singleDatePicker'] = true;
+		    picker.daterangepicker(options);
 
-		var daterange = picker.find('.daterange').daterangepicker(options, function(start, end, label){
-			startDate.val(start.format(datePickerFormat));
-        	endDate.val(end.format(datePickerFormat));
-		});
-		console.log(daterange)
-		startDate.val()
+		}
+        else{
+            var startDate = picker.find('input[name="start_date"]');
+            var endDate = picker.find('input[name="end_date"]');
+            var dropDirection = picker.attr('data-drop');
 
-		picker.find('button').on('click', function(){ daterange.focus(); });
+            if (startDate.val()){ options['startDate'] = startDate.val(); }
+            if (endDate.val()){ options['endDate'] = endDate.val(); }
+
+            startDate.val(moment(startDate.val(), datePickerFormat).format(datePickerFormat));
+            endDate.val(moment(endDate.val(), datePickerFormat).format(datePickerFormat));
+
+            var daterange = picker.find('.daterange').daterangepicker(options, function(start, end, label){
+                startDate.val(start.format(datePickerFormat));
+                endDate.val(end.format(datePickerFormat));
+            });
+
+            picker.find('button').on('click', function(){ daterange.focus(); });
+        }
+
 
 	});
 
