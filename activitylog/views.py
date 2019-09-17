f186f054c2c6c9ed6911df2ac5e2bf88f1818fee
Joseba S <jjoseba@gmail.com>
2019-09-17 11:54:24 +0200

    #650: Process activitylogs using tastypie resources instead of making an HTTP request

--- activitylog/views.py
+++ activitylog/views.py
@@ -1,7 +1,6 @@
 # oppia/activitylog/views.py
 
 import json
-import urllib
 
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password
@@ -18,38 +17,34 @@ from tastypie.models import ApiKey
 
 from activitylog.forms import UploadActivityLogForm
 from activitylog.models import UploadedActivityLog
+from api.resources import TrackerResource
+from helpers.api.tasty_resource import create_resource
+from oppia.permissions import user_can_upload
 from profile.models import UserProfile
+from quiz.api.resources import QuizAttemptResource
 
 
-def process_uploaded_trackers(request, user, user_api_key):
-    for tracker in user['trackers']:
-        url_comp = request.build_absolute_uri().split('/')
-        url = ('%(protocol)s//%(domain)s/api/v1/tracker/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key})
-        print(url)
-        data = json.dumps(tracker).encode("utf-8")
-        req = urllib.request.Request(url)
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
-        req.add_header('Accept', 'application/json; charset=utf-8')
-        try:
-            urllib.request.urlopen(req, data)
-            messages.info(request, _(u"Tracker activity for %(username)s added" % {'username': user['username']}))
-        except urllib.error.HTTPError:
-            messages.warning(request, _(u"Already uploaded: tracker activity for %(username)s added" % {'username': user['username']}), 'danger')
+def process_uploaded_trackers(request, trackers, user, user_api_key):
+    request.user = user
+    for tracker in trackers:
+        success, results = create_resource(TrackerResource, request, tracker)
+        if success:
+            messages.info(request, _(u"Tracker activity for %(username)s added" % {'username': user.username}))
+        else:
+            messages.warning(request, _(
+                u"Already uploaded: tracker activity %(uuid)s for %(username)s added" % {'username': user.username,
+                                                                                         'uuid': tracker.get('digest')}),'danger')
 
 
-def process_uploaded_quizresponses(request, user, user_api_key):
-    for quizattempt in user['quizresponses']:
-        url_comp = request.build_absolute_uri().split('/')
-        url = ('%(protocol)s//%(domain)s/api/v1/quizattempt/?username=%(username)s&api_key=%(api_key)s' % {'protocol': url_comp[0], 'domain': url_comp[2], 'username': user['username'], 'api_key': user_api_key.key})
-        data = json.dumps(quizattempt).encode("utf-8")
-        req = urllib.request.Request(url)
-        req.add_header('Content-Type', 'application/json; charset=utf-8')
-        req.add_header('Accept', 'application/json; charset=utf-8')
-        try:
-            urllib.request.urlopen(req, data)
-            messages.info(request, _(u"Quiz attempt for %(username)s added" % {'username': user['username']}))
-        except urllib.error.HTTPError:
-            messages.info(request, _(u"Already uploaded: quiz attempt for %(username)s added" % {'username': user['username']}))
+def process_uploaded_quizresponses(request, quiz_responses, user, user_api_key):
+    request.user = user
+    for quizattempt in quiz_responses:
+        success, results = create_resource(QuizAttemptResource, request, quizattempt)
+        if success:
+            messages.info(request, _(u"Quiz attempt for %(username)s added" % {'username': user.username}))
+        else:
+            messages.info(request,
+                          _(u"Already uploaded: quiz attempt for %(username)s added" % {'username': user.username}))
 
 
 def process_uploaded_file(request, json_data):
@@ -58,22 +53,23 @@ def process_uploaded_file(request, json_data):
             username = user['username']
             print(_(u"processing activity log for %s" % username))
 
+
             if User.objects.filter(username=username).count() == 0:
 
                 print(_(u"New user!"))
                 # User was registered offline, we create a new one
-                new_user = User(
+                req_user = User(
                     username=username,
                     email=user['email'],
                 )
 
-                new_user.password = user['password'] if 'password' in user else make_password(None)
-                new_user.first_name = user['firstname']
-                new_user.last_name = user['lastname']
-                new_user.save()
+                req_user.password = user['password'] if 'password' in user else make_password(None)
+                req_user.first_name = user['firstname']
+                req_user.last_name = user['lastname']
+                req_user.save()
 
                 user_profile = UserProfile()
-                user_profile.user = new_user
+                user_profile.user = req_user
                 user_profile.phone_number = user['phoneno'] if 'phoneno' in user else None
                 user_profile.job_title = user['jobtitle'] if 'jobtitle' in user else None
                 user_profile.organisation = user['organisation'] if 'organisation' in user else None
@@ -82,18 +78,20 @@ def process_uploaded_file(request, json_data):
                 messages.warning(request, _(
                     u"%(username)s did not exist previously, and was created." % {
                         'username': username}), 'danger')
+            else:
+                req_user = User.objects.filter(username=username).first()
 
             try:
-                user_api_key, created = ApiKey.objects.get_or_create(user__username=username)
+                user_api_key, created = ApiKey.objects.get_or_create(user=req_user)
                 if (created):
                     messages.warning(request, _(
                         u"Generated new ApiKey for %(username)s : %(apikey)s" % {
                             'username': username, 'apikey': user_api_key.key }), 'danger')
 
                 if 'trackers' in user:
-                    process_uploaded_trackers(request, user, user_api_key)
+                    process_uploaded_trackers(request, user['trackers'], req_user, user_api_key)
                 if 'quizresponses' in user:
-                    process_uploaded_quizresponses(request, user, user_api_key)
+                    process_uploaded_quizresponses(request, user['quizresponses'], req_user, user_api_key)
             except ApiKey.DoesNotExist:
                 messages.warning(request, _(u"%(username)s not found. Please check that this file is being uploaded to the correct server." % {'username': username}), 'danger')
                 print(_(u"No user api key found for %s" % user['username']))
