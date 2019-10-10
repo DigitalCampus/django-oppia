2b30fc9dd418eb6c00fbe895cdb7a95a53bda468
Alex Little <alex@digital-campus.org>
2019-10-10 15:28:34 +0300

    663: Initial version of DHIS2 export
    Task-Url: https://github.com/DigitalCampus/django-oppia/issues/issues/663

---
+++ tests/integrations/xapi/test_xapi_permissions.py
@@ -0,0 +1,78 @@
+# tests/integrations/dhis/test_dhis.py
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.test.client import Client
+
+from tests.utils import *
+from tests.user_logins import *
+
+class XAPIIntegrationViewsTest(TestCase):
+    fixtures = ['tests/test_user.json', 
+                'tests/test_oppia.json', 
+                'tests/test_quiz.json', 
+                'tests/test_permissions.json']
+
+    def setUp(self):
+        super(xAPIIntegrationViewsTest, self).setUp()
+        self.login_url = reverse('profile_login')
+    
+    def get_view(self, route, user=None):
+        if user is not None:
+            self.client.login(username=user['user'], password=user['password'])
+        return self.client.get(route)
+        
+    # test permissions for home
+    
+    def test_anon_cantview_integrations_home(self):
+        route = reverse('oppia_integrations_xapi_home')
+        res = self.get_view(route, None)
+        self.assertRedirects(res, self.login_url + '?next=/integrations/xapi/') 
+       
+    def test_admin_canview_integrations_home(self):
+        route = reverse('oppia_integrations_xapi_home')
+        res = self.get_view(route, ADMIN_USER)
+        self.assertEqual(res.status_code, 200)
+        
+    def test_staff_canview_integrations_home(self):
+        route = reverse('oppia_integrations_xapi_home')
+        res = self.get_view(route, STAFF_USER)
+        self.assertEqual(res.status_code, 200)
+
+    def test_student_cantview_integrations_home(self):
+        route = reverse('oppia_integrations_xapi_home')
+        res = self.get_view(route, NORMAL_USER)
+        self.assertRedirects(res, '/admin/login/?next=/integrations/xapi/')
+         
+    def test_user_with_canupload_integrations_home(self):
+        route = reverse('oppia_integrations_xapi_home')
+        res = self.get_view(route, TEACHER_USER)
+        self.assertRedirects(res, '/admin/login/?next=/integrations/xapi/')
+    
+    # test permissions and response for latest
+    
+    def test_anon_cantview_integrations_latest(self):
+        route = reverse('oppia_integrations_xapi_csv_export')
+        res = self.get_view(route, None)
+        self.assertRedirects(res, self.login_url + '?next=/integrations/xapi/export/') 
+       
+    def test_admin_canview_integrations_latest(self):
+        route = reverse('oppia_integrations_xapi_csv_export')
+        res = self.get_view(route, ADMIN_USER)
+        self.assertEqual(res.status_code, 200)
+        
+    def test_staff_canview_integrations_latest(self):
+        route = reverse('oppia_integrations_xapi_csv_export')
+        res = self.get_view(route, STAFF_USER)
+        self.assertEqual(res.status_code, 200)
+
+    def test_student_cantview_integrations_latest(self):
+        route = reverse('oppia_integrations_xapi_csv_export')
+        res = self.get_view(route, NORMAL_USER)
+        self.assertRedirects(res, '/admin/login/?next=/integrations/xapi/export/') 
+         
+    def test_user_with_canupload_integrations_latest(self):
+        route = reverse('oppia_integrations_xapi_csv_export')
+        res = self.get_view(route, TEACHER_USER)
+        self.assertRedirects(res, '/admin/login/?next=/integrations/xapi/export/') 
+    
+    
