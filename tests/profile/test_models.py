acc8abfd90aa172ec2077ecf5ffa3f86318778e0
Alex Little <alex@digital-campus.org>
2019-12-01 11:55:28 +0100

    #682 test models

--- tests/profile/test_models.py
+++ tests/profile/test_models.py
@@ -1,4 +1,98 @@
+from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
+from django.test import TestCase
 
-# test get_value
+from profile.models import CustomField, UserProfileCustomField
 
-# test multiple rows in userprofilecustomfield
+from tests.user_logins import NORMAL_USER
+
+
+class ProfileModelsTest(TestCase):
+    fixtures = ['tests/test_user.json',
+                'tests/test_oppia.json',
+                'tests/test_quiz.json']
+
+    def setUp(self):
+        super(ProfileModelsTest, self).setUp()
+
+    # test get_value string
+    def test_custom_field_get_value_str(self):
+        custom_field = CustomField(
+            id='str',
+            label='String',
+            required=True,
+            type='str')
+        custom_field.save()
+        user = User.objects.get(pk=NORMAL_USER['id'])
+        upcf = UserProfileCustomField(key_name=custom_field,
+                                      user=user,
+                                      value_str="my string")
+        upcf.save()
+
+        self.assertEqual(upcf.get_value(), "my string")
+        self.assertNotEqual(upcf.get_value(), True)
+        self.assertNotEqual(upcf.get_value(), False)
+        self.assertNotEqual(upcf.get_value(), None)
+        self.assertNotEqual(upcf.get_value(), 123)
+
+    # test get_value int
+    def test_custom_field_get_value_int(self):
+        custom_field = CustomField(
+            id='int',
+            label='Integer',
+            required=True,
+            type='int')
+        custom_field.save()
+        user = User.objects.get(pk=NORMAL_USER['id'])
+        upcf = UserProfileCustomField(key_name=custom_field,
+                                      user=user,
+                                      value_int=123)
+        upcf.save()
+
+        self.assertEqual(upcf.get_value(), 123)
+        self.assertNotEqual(upcf.get_value(), "123")
+        self.assertNotEqual(upcf.get_value(), True)
+        self.assertNotEqual(upcf.get_value(), False)
+        self.assertNotEqual(upcf.get_value(), None)
+        
+        
+    # get get value bool
+    def test_custom_field_get_value_bool(self):
+        custom_field = CustomField(
+            id='bool',
+            label='Boolean',
+            required=True,
+            type='bool')
+        custom_field.save()
+        user = User.objects.get(pk=NORMAL_USER['id'])
+        upcf = UserProfileCustomField(key_name=custom_field,
+                                      user=user,
+                                      value_bool=True)
+        upcf.save()
+
+        self.assertEqual(upcf.get_value(), True)
+        self.assertNotEqual(upcf.get_value(), "True")
+        self.assertNotEqual(upcf.get_value(), 123)
+        self.assertNotEqual(upcf.get_value(), False)
+        self.assertNotEqual(upcf.get_value(), None)
+    
+
+    # test multiple rows in userprofilecustomfield
+    def test_custom_field_multiple_rows(self):
+        custom_field = CustomField(
+            id='str',
+            label='String',
+            required=True,
+            type='str')
+        custom_field.save()
+        user = User.objects.get(pk=NORMAL_USER['id'])
+        upcf = UserProfileCustomField(key_name=custom_field,
+                                      user=user,
+                                      value_str="my string")
+        upcf.save()
+        
+        with self.assertRaises(IntegrityError):
+            upcf = UserProfileCustomField(key_name=custom_field,
+                                          user=user,
+                                          value_str="my other string")
+            upcf.save()
