962d8a98966dd7c95f0701c0e1f0c5db5ac8b91a
Alex Little <alex@digital-campus.org>
2019-12-15 17:33:47 +0200

    add test for cleanup_uploads

---
+++ tests/oppia/management/test_cleanup_uploads.py
@@ -0,0 +1,17 @@
+from io import StringIO
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class CleanUpUploadsTest(TestCase):
+    fixtures = ['tests/test_user.json',
+                'tests/test_oppia.json',
+                'tests/test_quiz.json',
+                'tests/test_permissions.json']
+
+    def setUp(self):
+        super(CleanUpUploadsTest, self).setUp()
+
+    def test_cleanup_uploads(self):
+        out = StringIO()
+        call_command('cleanup_uploads', stdout=out)
