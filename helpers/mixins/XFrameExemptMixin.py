554e5183f2a0a91af9f6007b5e08d1b37bd56aed
Alex Little <alex@digital-campus.org>
2019-12-31 12:48:49 +0200

    remove unused files

--- helpers/mixins/XFrameExemptMixin.py
+++
@@ -1,8 +0,0 @@
-from django.views import View
-from django.views.decorators.clickjacking import xframe_options_exempt
-
-
-class XFrameOptionsExemptMixin(View):
-    @xframe_options_exempt
-    def dispatch(self, *args, **kwargs):
-        return super(XFrameOptionsExemptMixin, self).dispatch(*args, **kwargs)
