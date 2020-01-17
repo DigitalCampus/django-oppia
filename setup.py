e5ed6aa7bbb5f8bc3f83fc2536c59bc12097fc63
Alex Little <alex@digital-campus.org>
2020-01-17 13:22:40 +0200

    update to django 2.2.9

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        "django == 2.2.8",
+        "django == 2.2.9",
         "django-tastypie == 0.14.2",
         "django-tablib >= 0.9.11",
         "django-crispy-forms >= 1.4.0",
