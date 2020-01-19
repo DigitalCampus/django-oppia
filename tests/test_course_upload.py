e79fa111b31f5ab7e7e7f1f2a365f387b25d52b6
Alex Little <alex@digital-campus.org>
2020-01-19 19:35:17 +0200

    add tests for uploading with gamification events

--- tests/test_course_upload.py
+++ tests/test_course_upload.py
@@ -1,6 +1,9 @@
 import pytest
 
 from django.urls import reverse
+from gamification.models import CourseGamificationEvent, \
+                                MediaGamificationEvent, \
+                                ActivityGamificationEvent
 from oppia.test import OppiaTestCase
 from oppia.models import Course, CoursePublishingLog
 from zipfile import BadZipfile
@@ -21,6 +24,8 @@ class CourseUploadTest(OppiaTestCase):
     course_no_sub_dir = file_root + 'test_course_no_sub_dir.zip'
     course_old_version = file_root + 'ncd1_old_course.zip'
     course_no_activities = file_root + 'test_course_no_activities.zip'
+    course_with_custom_points = file_root + 'ref-1.zip'
+    course_with_custom_points_updated = file_root + 'ref-1-updated.zip'
 
     @pytest.mark.xfail(reason="works on local but not on github workflows")
     def test_upload_template(self):
@@ -110,3 +115,73 @@ class CourseUploadTest(OppiaTestCase):
             self.assertEqual(200, response.status_code)
             course_log = CoursePublishingLog.objects.latest('log_date')
             self.assertEqual("no_activities", course_log.action)
+
+    @pytest.mark.xfail(reason="works on local but not on github workflows")
+    def test_course_with_custom_points(self):
+
+        course_game_events_start = CourseGamificationEvent. \
+            objects.all().count()
+        media_game_events_start = MediaGamificationEvent. \
+            objects.all().count()
+        activity_game_events_start = ActivityGamificationEvent. \
+            objects.all().count()
+
+        with open(self.course_with_custom_points, 'rb') as course_file:
+            self.client.force_login(self.admin_user)
+            response = self.client.post(reverse('oppia_upload'),
+                                        {'course_file': course_file})
+            course = Course.objects.latest('created_date')
+            self.assertRedirects(response,
+                                 reverse('oppia_upload2', args=[course.id]),
+                                 302,
+                                 200)
+
+        course_game_events_end = CourseGamificationEvent.objects.all().count()
+        self.assertEqual(course_game_events_start+10, course_game_events_end)
+
+        media_game_events_end = MediaGamificationEvent.objects.all().count()
+        self.assertEqual(media_game_events_start+4, media_game_events_end)
+
+        activity_game_events_end = ActivityGamificationEvent. \
+            objects.all().count()
+        self.assertEqual(activity_game_events_start+1,
+                         activity_game_events_end)
+
+    @pytest.mark.xfail(reason="works on local but not on github workflows")
+    def test_course_with_custom_points_updated(self):
+
+        with open(self.course_with_custom_points, 'rb') as course_file:
+            self.client.force_login(self.admin_user)
+            response = self.client.post(reverse('oppia_upload'),
+                                        {'course_file': course_file})
+            course = Course.objects.latest('created_date')
+            self.assertRedirects(response,
+                                 reverse('oppia_upload2', args=[course.id]),
+                                 302,
+                                 200)
+
+        course_game_events_start = CourseGamificationEvent. \
+            objects.all().count()
+        media_game_events_start = MediaGamificationEvent. \
+            objects.all().count()
+        activity_game_events_start = ActivityGamificationEvent. \
+            objects.all().count()
+
+        with open(self.course_with_custom_points_updated, 'rb') as course_file:
+            self.client.force_login(self.admin_user)
+            response = self.client.post(reverse('oppia_upload'),
+                                        {'course_file': course_file})
+            course = Course.objects.latest('created_date')
+            self.assertRedirects(response,
+                                 reverse('oppia_upload2', args=[course.id]),
+                                 302,
+                                 200)
+        course_game_events_end = CourseGamificationEvent.objects.all().count()
+        self.assertEqual(course_game_events_start, course_game_events_end)
+
+        media_game_events_end = MediaGamificationEvent.objects.all().count()
+        self.assertEqual(media_game_events_start, media_game_events_end)
+
+        activity_game_events_end = ActivityGamificationEvent. \
+            objects.all().count()
+        self.assertEqual(activity_game_events_start, activity_game_events_end)
