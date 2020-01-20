92bed7d5b2070134b1ee801e253fb26d4a79d09e
Alex Little <alex@digital-campus.org>
2020-01-20 21:20:03 +0200

    remove unused code

--- tests/gamification/test_forms.py
+++ tests/gamification/test_forms.py
@@ -1,4 +1,4 @@
-
+from django.urls import reverse
 from oppia.test import OppiaTestCase
 from gamification.forms import GamificationEventForm
 
@@ -10,6 +10,15 @@ class GamificationFormsTest(OppiaTestCase):
                 'tests/test_quiz.json',
                 'default_gamification_events.json']
 
-    # @TODO - complete this
-    def test_gamification_event_form(self):
-        GamificationEventForm()
+    def test_gamification_event_form_post(self):
+        data = {
+            'events-TOTAL_FORMS': 0,
+            'events-INITIAL_FORMS': 0,
+            'events-MIN_NUM_FORMS': 0,
+            'events-MAX_NUM_FORMS': 1000
+            }
+        url = reverse('oppia_gamification_edit_course', args=[1])
+
+        self.client.force_login(user=self.admin_user)
+        response = self.client.post(url, data)
+        self.assertEqual(200, response.status_code)
