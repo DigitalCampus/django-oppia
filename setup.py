6ebae9fa4462cb58d967f1e26e3fed0b122e5d89
Alex Little <alex@digital-campus.org>
2020-02-04 14:25:41 +0200

    new release branch and version no

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oppia',
-    version='0.12.4',
+    version='0.12.5',
     packages=[
         'oppia',
         'quiz',
