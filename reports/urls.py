03ce550aa1439d8be3d6ab15e10822583cd5199a
Alex Little <alex@digital-campus.org>
2020-03-02 14:00:43 +0100

    OPPIA-262 #closed

--- reports/urls.py
+++ reports/urls.py
@@ -5,9 +5,9 @@ from reports import views as oppia_reports_views
 
 urlpatterns = [
         url(r'^completion_rates/$',
-            oppia_reports_views.completion_rates,
+            oppia_reports_views.CompletionRates.as_view(),
             name="oppia_completion_rates"),
         url(r'^completion_rates/(?P<course_id>\d+)/$',
-            oppia_reports_views.course_completion_rates,
+            oppia_reports_views.CourseCompletionRates.as_view(),
             name="course_completion_rates"),
         ]
--- reports/views.py
+++ reports/views.py
@@ -4,7 +4,9 @@ from django.db.models import Sum, Count
 from django.http.response import Http404
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.utils.translation import ugettext_lazy as _
+from django.views.generic import TemplateView
 
 from oppia.models import Course
 from summary.models import UserCourseSummary
@@ -20,71 +22,77 @@ def menu_reports(request):
              'url': reverse('oppia_completion_rates')}]
 
 
-@staff_member_required
-def completion_rates(request):
 
-    courses = Course.objects.filter(is_draft=False,
-                                    is_archived=False).order_by('title')
+@method_decorator(staff_member_required, name='dispatch')
+class CompletionRates(TemplateView):
 
-    courses_list = []
-    course_stats = list(UserCourseSummary.objects
-                        .filter(course__in=courses)
-                        .values('course')
-                        .annotate(users=Count('user'),
-                                  completed=Sum('badges_achieved')))
+    def get(self, request):
 
-    for course in courses:
-        obj = {}
-        obj['course'] = course
+        courses = Course.objects.filter(is_draft=False,
+                                        is_archived=False).order_by('title')
 
-        for stats in course_stats:
-            if stats['course'] == course.id:
-                no_users = stats['users']
-                obj['enroled'] = no_users
-                if no_users > 0:
-                    obj['completion'] = (float(stats['completed'])
-                                         / float(no_users)) * 100
-                else:
-                    obj['completion'] = 0
-                # remove the element to optimize next searches
-                course_stats.remove(stats)
+        courses_list = []
+        course_stats = list(UserCourseSummary.objects
+                            .filter(course__in=courses)
+                            .values('course')
+                            .annotate(users=Count('user'),
+                                      completed=Sum('badges_achieved')))
 
-        courses_list.append(obj)
+        for course in courses:
+            obj = {}
+            obj['course'] = course
 
-    return render(request, 'reports/completion_rates.html',
-                  {'courses_list': courses_list})
+            for stats in course_stats:
+                if stats['course'] == course.id:
+                    no_users = stats['users']
+                    obj['enroled'] = no_users
+                    if no_users > 0:
+                        obj['completion'] = (float(stats['completed'])
+                                             / float(no_users)) * 100
+                    else:
+                        obj['completion'] = 0
+                    # remove the element to optimize next searches
+                    course_stats.remove(stats)
+
+            courses_list.append(obj)
+
+        return render(request, 'reports/completion_rates.html',
+                      {'courses_list': courses_list})
 
 
-@staff_member_required
-def course_completion_rates(request, course_id):
 
-    try:
-        course = Course.objects.get(pk=course_id)
-    except Course.DoesNotExist:
-        raise Http404
+@method_decorator(staff_member_required, name='dispatch')
+class CourseCompletionRates(TemplateView):
 
-    users_completed = []
-    users_incompleted = []
+    def get(self, request, course_id):
 
-    course_activities = course.get_no_activities()
-    users_stats = UserCourseSummary.objects \
-        .filter(course=course_id).order_by('user')
+        try:
+            course = Course.objects.get(pk=course_id)
+        except Course.DoesNotExist:
+            raise Http404
 
-    for user_stats in users_stats:
-        user_activities = user_stats.completed_activities
-        user_obj = {'user': user_stats.user}
-        user_obj['activities_completed'] = user_activities
-        user_obj['completion_percent'] = (user_activities
-                                          * 100
-                                          / course_activities)
-        if (user_activities >= course_activities):
-            users_completed.append(user_obj)
-        else:
-            users_incompleted.append(user_obj)
+        users_completed = []
+        users_incompleted = []
 
-    return render(request, 'reports/course_completion_rates.html',
-                  {'course': course,
-                   'users_enroled_count':
-                   len(users_completed) + len(users_incompleted),
-                   'users_completed': users_completed,
-                   'users_incompleted': users_incompleted})
+        course_activities = course.get_no_activities()
+        users_stats = UserCourseSummary.objects \
+            .filter(course=course_id).order_by('user')
+
+        for user_stats in users_stats:
+            user_activities = user_stats.completed_activities
+            user_obj = {'user': user_stats.user}
+            user_obj['activities_completed'] = user_activities
+            user_obj['completion_percent'] = (user_activities
+                                              * 100
+                                              / course_activities)
+            if (user_activities >= course_activities):
+                users_completed.append(user_obj)
+            else:
+                users_incompleted.append(user_obj)
+
+        return render(request, 'reports/course_completion_rates.html',
+                      {'course': course,
+                       'users_enroled_count':
+                       len(users_completed) + len(users_incompleted),
+                       'users_completed': users_completed,
+                       'users_incompleted': users_incompleted})
