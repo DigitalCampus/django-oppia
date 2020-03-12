1ff7ab02df6341f53a8fa8c4856708b9d088b546
Alex Little <alex@digital-campus.org>
2020-03-12 14:46:57 +0200

    improve efficiency of updating

--- quiz/migrations/0015_populate_time_taken.py
+++ quiz/migrations/0015_populate_time_taken.py
@@ -1,43 +1,31 @@
-import ast
+import json
 from django.db import migrations
-
-from oppia.uploader import clean_lang_dict
 
 
 def populate_time_taken(apps, schema_editor):
     quizattempt_model = apps.get_model("quiz", "QuizAttempt")
     tracker_model = apps.get_model("oppia", "Tracker")
 
-    quiz_attempts = quizattempt_model.objects.all() \
-        .exclude(instance_id='') \
-        .exclude(instance_id=None)
     print("\nUpdating quiz attempts for time taken."
           "\nThis might take a while, please be patient...")
+
+    trackers = tracker_model.objects.exclude(time_taken=0) \
+        .filter(type='quiz')
+
     counter = 1
-    for qa in quiz_attempts:
-        try:
-            tracker = tracker_model.objects.exclude(time_taken=0).get(
-                type='quiz',
-                data__icontains=qa.instance_id)
-            qa.time_taken = tracker.time_taken
-            qa.save()
-            print("{0}/{1} : {2} updated".format(counter,
-                                                 quiz_attempts.count(),
-                                                 qa.instance_id))
-        except tracker_model.DoesNotExist:
-            # pass/ignore
-            print("{0}/{1} : {2} not found in tracker table"
-                  .format(counter,
-                          quiz_attempts.count(),
-                          qa.instance_id))
-        except tracker_model.MultipleObjectsReturned:
-            # pass/ignore
-            print("{0}/{1} : {2} too many objects returned"
-                  .format(counter,
-                          quiz_attempts.count(),
-                          qa.instance_id))
+    for t in trackers:
+        json_data = json.loads(t.data)
+        if 'instance_id' in json_data:
+            quizattempt_model.objects \
+                .filter(instance_id=json_data['instance_id']) \
+                .update(time_taken=t.time_taken)
+
+        print("{0}/{1} : {2} updated".format(counter,
+                                             trackers.count(),
+                                             json_data['instance_id']))
 
         counter += 1
+
 
 def noop(app, schema_editor):
     # this migration is not reversible
