1280e3683f37469e76fefc6e748a98e7707cae26
Alex Little <alex@digital-campus.org>
2020-09-21 18:05:54 +0300

    extra tests for update_summaries command

--- tests/summary/test_summary_cron.py
+++ tests/summary/test_summary_cron.py
@@ -3,6 +3,7 @@ from django.core.management import call_command
 from io import StringIO
 
 from oppia.test import OppiaTestCase
+from oppia.models import Tracker, Points
 from settings.models import SettingProperties
 
 
@@ -103,3 +104,34 @@ class SummaryCronTest(OppiaTestCase):
         self.assertEqual(lock, 999)
         lock = SettingProperties.get_int('oppia_cron_lock', 999)
         self.assertEqual(lock, 999)
+
+    def test_summary_from_start(self):
+        call_command('update_summaries', '--fromstart', stdout=StringIO())
+
+        # check new details on pks
+        tracker_id = SettingProperties.get_int('last_tracker_pk', 0)
+        self.assertEqual(tracker_id, 1472216)
+        # this id is from the test_tracker data
+
+    def test_summary_invalid_latest_tracker(self):
+        SettingProperties.objects.update_or_create(key='last_tracker_pk',
+                                                   defaults={"int_value":
+                                                             2000000})
+        call_command('update_summaries', stdout=StringIO())
+
+        self.assertRaises(Tracker.DoesNotExist)
+        # check new details on pks
+        tracker_id = SettingProperties.get_int('last_tracker_pk', 0)
+        self.assertEqual(tracker_id, 2000000)
+
+    def test_summary_empty_tracker_table(self):
+        Tracker.objects.all().delete()
+        call_command('update_summaries', stdout=StringIO())
+
+        self.assertRaises(Tracker.DoesNotExist)
+
+    def test_summary_empty_points_table(self):
+        Points.objects.all().delete()
+        call_command('update_summaries', stdout=StringIO())
+
+        self.assertRaises(Points.DoesNotExist)
