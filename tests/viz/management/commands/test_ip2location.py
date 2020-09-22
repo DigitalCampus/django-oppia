4c574d84000d519d5019d907d7e12f97a9e4285a
Alex Little <alex@digital-campus.org>
2020-09-22 13:30:44 +0300

    update test for ip2location

--- tests/viz/management/commands/test_ip2location.py
+++ tests/viz/management/commands/test_ip2location.py
@@ -3,6 +3,7 @@ import pytest
 from django.core.management import call_command
 from io import StringIO
 from oppia.test import OppiaTransactionTestCase
+from viz.models import UserLocationVisualization
 
 
 class Ip2LocationTest(OppiaTransactionTestCase):
@@ -10,10 +11,19 @@ class Ip2LocationTest(OppiaTransactionTestCase):
                 'tests/test_oppia.json',
                 'tests/test_quiz.json',
                 'tests/test_permissions.json',
+                'tests/test_ip2location.json',
                 'default_badges.json']
 
     @pytest.mark.xfail(reason="works on local, but not on Github workflow")
     def test_ip2location_output(self):
-        out = StringIO()
-        call_command('ip2location', stdout=out)
-        self.assertEqual(u'completed', out.getvalue()[0:9])
+        
+        
+        old_count = UserLocationVisualization.objects.all().count()
+        call_command('ip2location', stdout=StringIO())
+    
+        self.assertRaises(UserLocationVisualization.DoesNotExist)
+        
+        new_count = UserLocationVisualization.objects.all().count()
+        
+        # @TODO - replace with old_count+2 once mock server set up
+        self.assertEqual(old_count, new_count)
