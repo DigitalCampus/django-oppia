c0d4fcf5010d6ae2844b2c755980136ff8c7e4b6
Alex Little <alex@digital-campus.org>
2020-10-27 17:50:53 +0200

    new release version

--- oppia/__init__.py
+++ oppia/__init__.py
@@ -1,5 +1,5 @@
 import warnings
 
-VERSION = (0, 12, 11, 'core', '1', 'release')
+VERSION = (0, 12, 12, 'core', '1', 'release')
 DEFAULT_IP_ADDRESS = None
 warnings.simplefilter('default')
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oppia',
-    version='0.12.11',
+    version='0.12.12',
     packages=[
         'oppia',
         'quiz',
