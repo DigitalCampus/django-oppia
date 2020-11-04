9a7aea67eab6e708fc5db1b15288f6184014c2ee
Alex Little <alex@digital-campus.org>
2020-11-04 19:55:01 +0200

    add icon for reports

--- reports/menu.py
+++ reports/menu.py
@@ -11,7 +11,9 @@ def menu_reports(request):
     #            'url':'/reports/2/'}]
     return [{'name': _(u'Completion Rates'),
              'description': _(u'Completion rates for each course'),
-             'url': reverse('reports:completion_rates')},
+             'url': reverse('reports:completion_rates'),
+             'icon': 'vertical_split'},
             {'name': _(u'Unique Users'),
              'description': _(u'Number of unique users, grouped by registration fields'),
-             'url': reverse('reports:unique_users')}]
+             'url': reverse('reports:unique_users'),
+             'icon': 'vertical_split'}]
