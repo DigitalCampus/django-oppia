3a2f61a05814910f2844fa4c2180223fca4f2b77
Joseba S <jjoseba@gmail.com>
2020-11-19 20:39:25 +0100

    oppia-422: Using Httpretty to mock IPStack server response

--- tests/viz/management/commands/test_cartodb_update.py
+++ tests/viz/management/commands/test_cartodb_update.py
@@ -1,6 +1,10 @@
 from io import StringIO
+
+import re
 from django.core.management import call_command
 from oppia.test import OppiaTestCase
+from settings import constants
+from settings.models import SettingProperties
 
 
 class CartoDBUpdateTest(OppiaTestCase):
@@ -11,7 +15,14 @@ class CartoDBUpdateTest(OppiaTestCase):
                 'default_badges.json',
                 'tests/test_course_permissions.json']
 
+    cartodb_valid_response = './oppia/fixtures/tests/ipstack/200_valid.json'
+    cartodb_uri_regex = re.compile("https?://(w+).cartodb.com/api/v2/sql??(?:&?[^=&]*=[^=&]*)*")
+
     def test_cartodb_output(self):
+        SettingProperties.set_string(constants.OPPIA_CARTODB_ACCOUNT, "digital-campus")
+        SettingProperties.set_string(constants.OPPIA_CARTODB_KEY, "7724a72cc5bb846b8cf02806c0112a5f7ef75b30")
+        SettingProperties.set_string(constants.OPPIA_HOSTNAME, "https://demo.oppia-mobile.org/")
+
         out = StringIO()
         call_command('cartodb_update', stdout=out)
         # API KEY won't be valid
--- tests/viz/management/commands/test_ip2location.py
+++ tests/viz/management/commands/test_ip2location.py
@@ -1,8 +1,14 @@
+import httpretty
 import pytest
+import re
 
 from django.core.management import call_command
 from io import StringIO
+
+from settings import constants
 from oppia.test import OppiaTransactionTestCase
+from settings.models import SettingProperties
+from tests.utils import get_file_contents
 from viz.models import UserLocationVisualization
 
 
@@ -15,8 +21,16 @@ class Ip2LocationTest(OppiaTransactionTestCase):
                 'default_badges.json',
                 'tests/test_course_permissions.json']
 
-    @pytest.mark.xfail(reason="works on local, but not on Github workflow")
+    ipstack_valid_response = './oppia/fixtures/tests/ipstack/200_valid.json'
+    ipstack_uri_regex = re.compile("https?://api.ipstack.com/??(?:&?[^=&]*=[^=&]*)*")
+
+    @httpretty.activate
     def test_ip2location_output(self):
+
+        ipstack_response = get_file_contents(self.ipstack_valid_response)
+        httpretty.register_uri( httpretty.GET, self.ipstack_uri_regex, body=ipstack_response)
+
+        SettingProperties.set_string(constants.OPPIA_IPSTACK_APIKEY, "FAKE_APIKEY")
 
         old_count = UserLocationVisualization.objects.all().count()
         call_command('ip2location', stdout=StringIO())
@@ -25,5 +39,4 @@ class Ip2LocationTest(OppiaTransactionTestCase):
 
         new_count = UserLocationVisualization.objects.all().count()
 
-        # @TODO - replace with old_count+2 once mock server set up
-        self.assertEqual(old_count, new_count)
+        self.assertEqual(old_count+2, new_count)
