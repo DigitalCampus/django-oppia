e8b2ca45097800a1a78dd6a05176067520420381
Alex Little <alex@digital-campus.org>
2021-10-18 18:42:17 +0200

    remove unused setting

--- oppiamobile/settings.py
+++ oppiamobile/settings.py
@@ -194,7 +194,6 @@ OPPIA_MAX_UPLOAD_SIZE = 5242880  # max course file upload size - in bytes
 OPPIA_VIDEO_FILE_TYPES = ("video/m4v", "video/mp4", "video/3gp", "video/3gpp")
 OPPIA_AUDIO_FILE_TYPES = ("audio/mpeg", "audio/amr", "audio/mp3")
 OPPIA_MEDIA_FILE_TYPES = OPPIA_VIDEO_FILE_TYPES + OPPIA_AUDIO_FILE_TYPES
-OPPIA_MEDIA_IMAGE_FILE_TYPES = ("image/png", "image/jpeg")
 
 OPPIA_UPLOAD_TRACKER_FILE_TYPES = [("application/json")]
 
