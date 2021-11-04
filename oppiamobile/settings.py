8ab676b26094a521e1082582d07889c9b3d69d8e
Alex Little <alex@digital-campus.org>
2021-11-04 15:15:28 +0200

    fix #OPPIA-868

--- oppiamobile/settings.py
+++ oppiamobile/settings.py
@@ -87,6 +87,31 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles'
 ]
 
+# Logging (automated error emails)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}   
+
 TIME_ZONE = 'UTC'
 USE_TZ = True
 SITE_ID = 1
