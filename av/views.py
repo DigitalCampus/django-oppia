8e27a58b09cbcee90300121437cf04e7584a060e
Alex Little <alex@digital-campus.org>
2022-03-03 13:56:16 +0200

    fix code smell for unused variables

--- av/views.py
+++ av/views.py
@@ -53,9 +53,7 @@ class CourseMediaList(ListView, ListItemUrlMixin, AjaxTemplateResponseMixin):
 
 
 def download_media_file(request, course_id, media_id):
-    course = can_view_course(request, course_id)
     media = Media.objects.get(pk=media_id)
-    uploaded = UploadedMedia.objects.filter(md5=media.digest).first()
 
     response = HttpResponse()
     response['Content-Disposition'] = 'attachment; filename="%s"' \
