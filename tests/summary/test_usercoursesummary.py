62a770181d28f56971c738fc2fd78cfc9cfa1c95
Alex Little <alex@digital-campus.org>
2022-03-18 13:20:01 +0200

    initial test script and test course files

---
+++ tests/summary/test_usercoursesummary.py
@@ -0,0 +1,112 @@
+from django.core.management import call_command
+
+from io import StringIO
+
+from oppia.test import OppiaTestCase
+from oppia.models import Course
+from summary.models import UserCourseSummary
+
+
+'''
+Test Data:
+
+3 versions of course
+v1 - page 1v1, page 2v1, page 3v1, quiz 1v1, feedback 1v1
+v2 - page 1v2, page 2v1, page 3v1, quiz 1v2, feedback 1v1, page 4v1
+v3 - page 1v2, page 2v1, page 3v1, quiz 1v3, feedback 1v2
+
+
+3 Users
+User A
+course-v1 - page 1v1, page 2v1 completed, quiz 1v1 attempted twice, feedback 1v1 completed
+    expected: total_activity - 5, completed_activities - 3, total_activity_current - 5, total_activity_previous - 0
+course-v2 - page 1v2, page 2v1 completed, quiz 1v2 passed, page 4v1 completed
+    expected: total_activity - 9, completed_activities - 5, total_activity_current - 6, total_activity_previous - 3
+course-v3 - page 3v1 completed, feedback 1v2 completed
+    expected: total_activity - 11, completed_activities - 4, total_activity_current - 5 total_activity_previous - 6 
+
+User B
+course-v1 - no activity
+    expected: total_activity - 0, completed_activities - 0, total_activity_current - 0, total_activity_previous - 0 
+course-v2 - p1v2 completed twice, quiz1v2 complete (passed)
+    expected: total_activity - 3, completed_activities - 2, total_activity_current - 3, total_activity_previous - 0
+course-v3 - page2v1 complete, feedback 1v2 complete, quiz 1v3 passed
+    expected: total_activity - 6, completed_activities - 4, total_activity_current - 5, total_activity_previous - 1
+
+User C
+course-v1 - no activity
+    expected: total_activity - 0, completed_activities - 0, total_activity_current - 0, total_activity_previous - 0 
+course-v2 - no activity
+    expected: total_activity - 0, completed_activities - 0, total_activity_current - 0, total_activity_previous - 0 
+course-v3 - page 1v2 twice, page 2v1, quiz 1v3 passed
+    expected: total_activity - 4, completed_activities - 3, total_activity_current - 4, total_activity_previous - 0
+
+'''
+    
+COURSE_SHORTNAME = "oppia800course"
+
+class UserCourseSummaryTestCourseV1(OppiaTestCase):
+    
+    fixtures = []
+    
+    def setUp(self):
+        self.course = Course.objects.get(shortname=COURSE_SHORTNAME)
+        self.user_a = User.objects.get(username="user_a")
+        self.user_b = User.objects.get(username="user_b")
+        self.user_c = User.objects.get(username="user_c")
+
+    def test_incremental(self):
+        call_command('update_summaries', stdout=StringIO())
+        self.check_summary_values()
+        
+    def test_fromstart(self):
+        call_command('update_summaries', '--fromstart', stdout=StringIO())
+        self.check_summary_values()
+    
+    def check_summary_values(self):
+         # User A
+        user_a_summary = UserCourseSummary.objects.get(
+            course=self.course,
+            user=self.user_a)
+        self.assertEqual(5, user_a_summary.total_activity)
+        self.assertEqual(3, user_a_summary.completed_activities)
+        self.assertEqual(5, user_a_summary.total_activity_current)
+        self.assertEqual(0, user_a_summary.total_activity_previous)
+        
+        # User B
+        user_b_summary = UserCourseSummary.objects.get(
+            course=self.course,
+            user=self.user_b)
+        self.assertEqual(0, user_b_summary.total_activity)
+        self.assertEqual(0, user_b_summary.completed_activities)
+        self.assertEqual(0, user_b_summary.total_activity_current)
+        self.assertEqual(0, user_b_summary.total_activity_previous)
+
+        # User C
+        user_c_summary = UserCourseSummary.objects.get(
+            course=self.course,
+            user=self.user_c)
+        self.assertEqual(0, user_c_summary.total_activity)
+        self.assertEqual(0, user_c_summary.completed_activities)
+        self.assertEqual(0, user_c_summary.total_activity_current)
+        self.assertEqual(0, user_c_summary.total_activity_previous)
+        
+class UserCourseSummaryTestCourseV2(OppiaTestCase):
+    
+    fixtures = []
+    
+
+class UserCourseSummaryTestCourseV3(OppiaTestCase):
+    
+    fixtures = []
+    
+    
+    '''
+    to test
+    
+    # 
+    
+    test each with and without course version info existing in trackers
+    run on incremental updates and also on fromstart - exactly the same expected
+    output for each
+    '''
