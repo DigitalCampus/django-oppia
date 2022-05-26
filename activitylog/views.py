3d284a7f0898d51d1b67073f1716f3c17822f379
Joseba S <jjoseba@gmail.com>
2022-05-26 15:10:41 +0200

    oppia-1199: Reduce activitylogs form cognitive complexity and transformed in CBV

--- activitylog/views.py
+++ activitylog/views.py
@@ -3,64 +3,52 @@ import json
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
-from django.urls import reverse
-from django.http import HttpResponseRedirect, \
-    HttpResponse, \
-    HttpResponseBadRequest, HttpRequest
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpRequest
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import FormView
 from tastypie.models import ApiKey
 
 from activitylog.forms import UploadActivityLogForm
 from activitylog.models import UploadedActivityLog
-from helpers.messages import MessagesDelegate
-from oppia.permissions import user_can_upload
 from api.resources.tracker import TrackerResource
 from helpers.api.tasty_resource import create_resource
+from helpers.messages import MessagesDelegate
+from oppia.permissions import user_can_upload
 from profile.models import UserProfile
 from quiz.api.resources import QuizAttemptResource
-
-from django.views.generic import TemplateView
-
 from settings import constants
 from settings.models import SettingProperties
 
 
 @method_decorator(user_can_upload, name='dispatch')
-class UploadView(TemplateView):
+class UploadView(FormView):
 
-    def get(self, request):
-        form = UploadActivityLogForm()
-        return render(request, 'common/upload.html',
-                      {'form': form,
-                       'title': _(u'Upload Activity Log')})
+    form_class = UploadActivityLogForm
+    template_name = 'common/upload.html'
+    extra_context = { 'title': _(u'Upload Activity Log') }
+    success_url = reverse_lazy('activitylog:upload_success')
 
-    def post(self, request):
-        form = UploadActivityLogForm(request.POST, request.FILES)
-        if form.is_valid():
-            activity_log_file = request.FILES["activity_log_file"]
+    def form_valid(self, form):
+        activity_log_file = self.request.FILES["activity_log_file"]
 
-            # save activity_log_file
-            uploaded_activity_log = \
-                UploadedActivityLog(create_user=request.user,
-                                    file=activity_log_file)
-            uploaded_activity_log.save()
+        # save activity_log_file
+        uploaded_activity_log = UploadedActivityLog(create_user=self.request.user, file=activity_log_file)
+        uploaded_activity_log.save()
 
-            # open file and process
-            with open(uploaded_activity_log.file.path, 'rb') as file:
-                file_data = file.read()
-                messages_delegate = MessagesDelegate(request)
-                success = process_activitylog(messages_delegate, file_data)
-                if success:
-                    return HttpResponseRedirect(
-                        reverse('activitylog:upload_success'))
+        # open file and process
+        with open(uploaded_activity_log.file.path, 'rb') as file:
+            file_data = file.read()
+            messages_delegate = MessagesDelegate(self.request)
+            success = process_activitylog(messages_delegate, file_data)
 
-        return render(request, 'common/upload.html',
-                      {'form': form,
-                       'title': _(u'Upload Activity Log')})
+        if success:
+            return super().form_valid(form)
+        else:
+            return super().form_invalid(form)
 
 
 def process_activitylog(messages_delegate, file_contents):
@@ -82,13 +70,11 @@ def process_uploaded_trackers(messages_delegate, trackers, user):
         if success:
             messages_delegate.info(
                           _(u"Tracker activity %(uuid)s for %(username)s added"
-                            % {'username': user.username,
-                               'uuid': tracker.get('digest')}))
+                            % {'username': user.username, 'uuid': tracker.get('digest')}))
         else:
             messages_delegate.warning(
                 _(u"Already uploaded: tracker activity %(uuid)s for "
-                  "%(username)s" % {'username': user.username,
-                                    'uuid': tracker.get('digest', None)}),
+                  "%(username)s" % {'username': user.username, 'uuid': tracker.get('digest', None)}),
                 'danger')
 
 
@@ -97,9 +83,7 @@ def process_uploaded_quizresponses(messages_delegate, quiz_responses, user):
     request = HttpRequest()
     request.user = user
     for quizattempt in quiz_responses:
-        success, results = create_resource(QuizAttemptResource,
-                                           request,
-                                           quizattempt)
+        success, results = create_resource(QuizAttemptResource, request, quizattempt)
         if success:
             messages_delegate.info(_(u"Quiz attempt for %(username)s added"
                                      % {'username': user.username}))
@@ -113,44 +97,27 @@ def process_uploaded_file(messages_delegate, json_data):
     if 'users' in json_data:
         for user in json_data['users']:
             username = user['username']
-            req_user, user_profile = \
-                get_user_from_uploaded_log(messages_delegate, user)
-
-            if 'job_title' in user and user['job_title']:
-                user_profile.job_title = user['job_title']
-            if 'organisation' in user and user['organisation']:
-                user_profile.organisation = user['organisation']
-            if 'phoneno' in user and user['phoneno']:
-                user_profile.phone_number = user['phoneno']
-            user_profile.save()
-            user_profile.update_customfields(user)
+            req_user = get_user_from_uploaded_log(messages_delegate, user)
+            create_or_update_userprofile(messages_delegate, req_user, user)
 
             try:
-                user_api_key, created = ApiKey.objects \
-                    .get_or_create(user=req_user)
+                user_api_key, created = ApiKey.objects.get_or_create(user=req_user)
                 if (created):
                     messages_delegate.warning(
-                                     _(u"Generated new ApiKey for \
-                                       %(username)s : %(apikey)s" % {
-                                           'username': username,
-                                           'apikey': user_api_key.key}),
-                                     'danger')
+                         _(u"Generated new ApiKey for %(username)s : %(apikey)s" % {
+                               'username': username,
+                               'apikey': user_api_key.key}),
+                         'danger')
 
                 if 'trackers' in user:
-                    process_uploaded_trackers(messages_delegate,
-                                              user['trackers'],
-                                              req_user)
+                    process_uploaded_trackers(messages_delegate, user['trackers'], req_user)
                 if 'quizresponses' in user:
-                    process_uploaded_quizresponses(messages_delegate,
-                                                   user['quizresponses'],
-                                                   req_user)
+                    process_uploaded_quizresponses(messages_delegate, user['quizresponses'], req_user)
+
             except ApiKey.DoesNotExist:
                 messages_delegate.warning(
-                                 _(u"%(username)s not found. Please \
-                                   check that this file is being uploaded to \
-                                   the correct server."
-                                   % {'username': username}),
-                                 'danger')
+                     _(u"%(username)s not found. Please check that this file is being uploaded to \
+                       the correct server." % {'username': username}), 'danger')
 
 
 def get_user_from_uploaded_log(messages_delegate, user):
@@ -162,47 +129,49 @@ def get_user_from_uploaded_log(messages_delegate, user):
 
     if not User.objects.filter(username=username).exists():
         # User was registered offline, we create a new one
-        req_user = User(
-            username=username,
-            email=user['email'] if 'email' in user
-            and user['email'] is not None else '',
-        )
+        req_user = User(username=username, email=user.get('email', '') )
 
-        req_user.password = user['password'] \
-            if 'password' in user else make_password(None)
+        req_user.password = user.get('password', make_password(None))
         req_user.first_name = user.get('firstname', '')
         req_user.last_name = user.get('lastname', '')
         req_user.save()
 
-        user_profile = UserProfile(user=req_user)
         messages_delegate.warning(
-            _(u"%(username)s did not exist previously, and was created."
-              % {'username': username}),
-            'danger')
+            _(u"%(username)s did not exist previously, and was created." %
+            {'username': username}), 'danger')
+
     else:
         req_user = User.objects.filter(username=username).first()
-        user_profile, created = UserProfile.objects.get_or_create(
-            user=req_user)
 
-    return req_user, user_profile
+    return req_user
+
+
+def create_or_update_userprofile(messages_delegate, req_user, user_data):
+    user_profile, created = UserProfile.objects.get_or_create(user=req_user)
+
+    if 'job_title' in user_data and user_data['job_title']:
+        user_profile.job_title = user_data['job_title']
+    if 'organisation' in user_data and user_data['organisation']:
+        user_profile.organisation = user_data['organisation']
+    if 'phoneno' in user_data and user_data['phoneno']:
+        user_profile.phone_number = user_data['phoneno']
+
+    user_profile.save()
+    user_profile.update_customfields(user_data)
 
 
 def validate_server(messages_delegate, data):
 
     if 'server' in data:
-        server_url = SettingProperties.get_string(constants.OPPIA_HOSTNAME,
-                                                  "localhost")
+        server_url = SettingProperties.get_string(constants.OPPIA_HOSTNAME, 'localhost')
         if data['server'].startswith(server_url):
             return True
         else:
             print('Different tracker server: {}'.format(data['server']))
-            messages_delegate.warning(_(
-                "The server in the activity log file does not match with the \
-                current one"))
+            messages_delegate.warning( _('The server in the activity log file does not match with the current one'))
             return False
     else:
-        messages_delegate.warning(_(
-            "The activity log file seems to be in a wrong format"))
+        messages_delegate.warning(_('The activity log file seems to be in a wrong format'))
         return False
 
 
