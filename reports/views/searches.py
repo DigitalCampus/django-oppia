7d18ab4b0ad185be3062f3d08b19f851f18801ec
Joseba S <jjoseba@gmail.com>
2022-06-21 12:02:39 +0200

    oppia-1176: Update all report views to the new ReportTemplateView implementation

--- reports/views/searches.py
+++ reports/views/searches.py
@@ -1,24 +1,19 @@
 import json
-
-from django.contrib.admin.views.decorators import staff_member_required
-from django.db.models import Sum
-from django.db.models.functions import TruncMonth, TruncYear
-from django.shortcuts import render
-from django.utils.decorators import method_decorator
-
 from operator import itemgetter
 
+from django.db.models import Sum
+from django.db.models.functions import TruncMonth, TruncYear
+
 from oppia.models import Tracker
-
 from reports.views.base_report_template import BaseReportTemplateView
-
 from summary.models import CourseDailyStats
 
 
-@method_decorator(staff_member_required, name='dispatch')
 class SearchesView(BaseReportTemplateView):
 
-    def process(self, request, form, start_date, end_date):
+    template_name = 'reports/searches.html'
+
+    def get_graph_data(self, start_date, end_date):
         searches = CourseDailyStats.objects \
             .filter(day__gte=start_date,
                     day__lte=end_date,
@@ -34,24 +29,24 @@ class SearchesView(BaseReportTemplateView):
                     type='search') \
             .aggregate(total=Sum('total')) \
             .get('total', 0)
+
         if previous_searches is None:
             previous_searches = 0
-        return render(request, 'reports/searches.html',
-                      {'form': form,
-                       'searches': searches,
-                       'previous_searches':
-                       previous_searches})
+
+        return {
+            'searches': searches,
+            'previous_searches': previous_searches
+        }
 
 
-@method_decorator(staff_member_required, name='dispatch')
 class SearchTermView(BaseReportTemplateView):
 
-    def process(self, request, form, start_date, end_date):
-        searches = Tracker.objects.filter(type='search',
-                                          user__is_staff=False,
+    template_name = 'reports/search_terms.html'
+
+    def get_graph_data(self, start_date, end_date):
+        searches = Tracker.objects.filter(type='search', user__is_staff=False,
                                           submitted_date__gte=start_date,
                                           submitted_date__lte=end_date)
-
         search_terms = []
 
         for search in searches:
@@ -73,9 +68,8 @@ class SearchTermView(BaseReportTemplateView):
         search_terms = sorted(search_terms,
                               key=itemgetter('count'),
                               reverse=True)
-        return render(request, 'reports/search_terms.html',
-                      {'form': form,
-                       'search_terms': search_terms})
+
+        return {'search_terms': search_terms}
 
     def get_query_term(self, data):
         try:
