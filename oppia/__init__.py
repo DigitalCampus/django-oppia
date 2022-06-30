47f51216a9e80e4e8b13b6e1de5c059d3a71df9c
Alex Little <alex@digital-campus.org>
2022-06-30 09:28:03 +0300

    next release branch

--- oppia/__init__.py
+++ oppia/__init__.py
@@ -1,5 +1,5 @@
 import warnings
 
-VERSION = (0, 14, 1, 'core', '1', 'release')
+VERSION = (0, 14, 2, 'core', '1', 'release')
 DEFAULT_IP_ADDRESS = None
 warnings.simplefilter('default')
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-oppia',
-    version='0.14.0',
+    version='0.14.2',
     packages=[
         'oppia',
         'quiz',
