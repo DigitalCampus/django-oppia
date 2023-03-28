937f633c2c8bd7e3ee08f8ff38eac071d3678d94
Alex Little <alex@digital-campus.org>
2023-03-28 11:04:15 +0300

    additional checks for API v2 response tests

--- tests/api/v2/test_login.py
+++ tests/api/v2/test_login.py
@@ -46,8 +46,24 @@ class UserResourceTest(ResourceTestCaseMixin, TestCase):
         self.assertTrue('organisation' in response_data)
         self.assertTrue('first_name' in response_data)
         self.assertTrue('last_name' in response_data)
-        # check it doesn't contain the password
-        self.assertFalse('password' in response_data)
+        self.assertFalse('password' in response_data) # check it doesn't contain the password
+        self.assertTrue('cohorts' in response_data)
+        self.assertTrue('badging' in response_data)
+        self.assertTrue('scoring' in response_data)
+        self.assertTrue('username' in response_data)
+        self.assertTrue('metadata' in response_data)
+        self.assertTrue('resource_uri' in response_data)
+        
+        self.assertEqual(100, response_data['points'])
+        self.assertEqual(0, response_data['badges'])
+        self.assertTrue(response_data['badging'])
+        self.assertTrue(response_data['scoring'])
+        self.assertEqual("demo@me.com", response_data['email'])
+        self.assertEqual("", response_data['organisation'])
+        self.assertEqual("", response_data['job_title'])
+        self.assertEqual("demo", response_data['first_name'])
+        self.assertEqual("user", response_data['last_name'])
+        self.assertEqual("demo", response_data['username'])
 
     # check inactive user can't access
     def test_inactive_username(self):
--- tests/api/v2/test_points.py
+++ tests/api/v2/test_points.py
@@ -13,8 +13,7 @@ class PointsResourceTest(ResourceTestCaseMixin, TestCase):
                 'tests/test_leaderboard.json',
                 'default_badges.json',
                 'default_gamification_events.json',
-                'tests/usercoursesummary/course_tracker_v3.json'
-                ]
+                'tests/usercoursesummary/course_tracker_v3.json']
 
     STR_LEADERBOARD_FILTERED_URL = "/api/v2/leaderboard/"
 
@@ -51,7 +50,6 @@ class PointsResourceTest(ResourceTestCaseMixin, TestCase):
 
     # check a valid get
     def test_get_points(self):
-        
         user = User.objects.get(username='demo')
         api_key = get_api_key(user=user)
         auth_header = self.create_apikey(username=user.username, api_key=api_key.key)
@@ -74,7 +72,35 @@ class PointsResourceTest(ResourceTestCaseMixin, TestCase):
         self.assertHttpOK(response)
         self.assertValidJSON(response.content)
         json_data = json.loads(response.content)
+        
+        self.assertTrue('generated_date' in json_data)
+        self.assertTrue('server' in json_data)
+        self.assertTrue('leaderboard' in json_data)
+        
         self.assertEqual(2037, len(json_data['leaderboard']))
+        
+        leader = json_data['leaderboard'][0]
+        self.assertTrue('position' in leader)
+        self.assertTrue('username' in leader)
+        self.assertTrue('first_name' in leader)
+        self.assertTrue('last_name' in leader)
+        self.assertTrue('points' in leader)
+        self.assertTrue('badges' in leader)
+
+        self.assertEqual(1, leader['position'])
+        self.assertEqual("user4847", leader['username'])
+        self.assertEqual("User", leader['first_name'])
+        self.assertEqual("4847", leader['last_name'])
+        self.assertEqual(169437, leader['points'])
+        self.assertEqual(7, leader['badges'])
+        
+        position567 = json_data['leaderboard'][566]
+        self.assertEqual(567, position567['position'])
+        self.assertEqual("user4856", position567['username'])
+        self.assertEqual("User", position567['first_name'])
+        self.assertEqual("4856", position567['last_name'])
+        self.assertEqual(720, position567['points'])
+        self.assertEqual(1, position567['badges'])
 
     # check top user
     def test_get_leaderboard_filtered_position1_user(self):
@@ -86,7 +112,22 @@ class PointsResourceTest(ResourceTestCaseMixin, TestCase):
         self.assertValidJSON(response.content)
         json_data = json.loads(response.content)
         self.assertEqual(21, len(json_data['leaderboard']))
+        
+        leader = json_data['leaderboard'][0]
+        self.assertTrue('position' in leader)
+        self.assertTrue('username' in leader)
+        self.assertTrue('first_name' in leader)
+        self.assertTrue('last_name' in leader)
+        self.assertTrue('points' in leader)
+        self.assertTrue('badges' in leader)
 
+        self.assertEqual(1, leader['position'])
+        self.assertEqual("user4847", leader['username'])
+        self.assertEqual("User", leader['first_name'])
+        self.assertEqual("4847", leader['last_name'])
+        self.assertEqual(169437, leader['points'])
+        self.assertEqual(7, leader['badges'])
+        
     # Check bottom of top users
     def test_get_leaderboard_filtered_position20_user(self):
         auth_header = self.create_apikey(username="user3263", api_key="1234")
@@ -97,6 +138,21 @@ class PointsResourceTest(ResourceTestCaseMixin, TestCase):
         self.assertValidJSON(response.content)
         json_data = json.loads(response.content)
         self.assertEqual(40, len(json_data['leaderboard']))
+        
+        position_x = json_data['leaderboard'][37]
+        self.assertTrue('position' in position_x)
+        self.assertTrue('username' in position_x)
+        self.assertTrue('first_name' in position_x)
+        self.assertTrue('last_name' in position_x)
+        self.assertTrue('points' in position_x)
+        self.assertTrue('badges' in position_x)
+
+        self.assertEqual(38, position_x['position'])
+        self.assertEqual("user2622", position_x['username'])
+        self.assertEqual("User", position_x['first_name'])
+        self.assertEqual("2622", position_x['last_name'])
+        self.assertEqual(8446, position_x['points'])
+        self.assertEqual(7, position_x['badges'])
 
     def test_get_leaderboard_filtered_position21_user(self):
         auth_header = self.create_apikey(username="user3342", api_key="1234")
