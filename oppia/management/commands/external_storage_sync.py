6105a76945f2890b9abd30b1fac740bacd2955cf
Alex Little <alex@digital-campus.org>
2024-07-18 13:58:04 +0300

    OPPIA-1661 & OPPIA-1662

--- oppia/management/commands/external_storage_sync.py
+++ oppia/management/commands/external_storage_sync.py
@@ -1,4 +1,6 @@
 import os.path
+import pathlib
+from os import listdir
 from shutil import copyfile
 
 from django.conf import settings
@@ -15,6 +17,7 @@ class Command(BaseCommand):
             return
 
         # Sync media
+        # add any local media that are missing from external
         uploaded_media = UploadedMedia.objects.all()
         for um in uploaded_media:
             print("checking %s" % um.filename())
@@ -25,3 +28,16 @@ class Command(BaseCommand):
                 print("copied to %s" % external_full_path)
             else:
                 print("File already synced")
+
+        # Remove any external media that are no longer used
+        external_files = [f for f in listdir(settings.OPPIA_EXTERNAL_STORAGE_MEDIA_ROOT)
+                          if os.path.isfile(os.path.join(settings.OPPIA_EXTERNAL_STORAGE_MEDIA_ROOT, f))]
+        for ef in external_files:
+            print("checking %s" % ef)
+            filename_to_check = "/" + ef
+            um = UploadedMedia.objects.filter(file__endswith=filename_to_check)
+            if um.count() == 0:
+                #file_to_remove = pathlib.Path("/tmp/<file_name>.txt")
+                #file_to_remove.unlink()
+                print("file no longer used so removed")
+
